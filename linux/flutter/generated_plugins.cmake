807b29a231c680bef1af645bb4041e094461b77f
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-08-08 16:44:20 +0300

    fix: cold start for deep links (#1814)
    ## Description
    Fixes cold start with deep links
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Task ID
    3527
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_saver
   file_selector_linux
   flutter_secure_storage_linux
+  gtk
   screen_retriever_linux
   sentry_flutter
   sqlite3_flutter_libs
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import connectivity_plus
 import device_info_plus
 import ffmpeg_kit_flutter
@@ -35,6 +36,7 @@ import wakelock_plus
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
