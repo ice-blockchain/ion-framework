0729b458172d0b5e50c1cefa2c5ecba8a2136690
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-08-08 13:34:01 +0300

    feat: use appsflyer for reports & cleanup unused report reason

--- lib/app/features/user/providers/report_notifier.m.dart
+++ lib/app/features/user/providers/report_notifier.m.dart
@@ -4,6 +4,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ion/app/constants/emails.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.f.dart';
 import 'package:ion/app/features/user/model/user_metadata.f.dart';
+import 'package:ion/app/services/deep_link/deep_link_service.r.dart';
 import 'package:ion/app/services/mail/mail.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -15,8 +16,6 @@ sealed class ReportReason with _$ReportReason {
   const factory ReportReason.user({required String pubkey}) = ReportReasonUser;
   const factory ReportReason.content({required EventReference eventReference}) =
       ReportReasonContent;
-  const factory ReportReason.conversation({required String conversationId}) =
-      ReportReasonConversation;
 }
 
 @riverpod
@@ -32,18 +31,25 @@ class ReportNotifier extends _$ReportNotifier {
       await sendEmail(
         receiver: Emails.support,
         subject: reportSubject,
-        body: _getReportBody(reason),
+        body: await _getReportBody(reason),
       );
     });
   }
 
-  String _getReportBody(ReportReason reason) {
+  Future<String> _getReportBody(ReportReason reason) async {
+    final deepLinkService = ref.read(deepLinkServiceProvider);
+
+    final encodedReason = switch (reason) {
+      ReportReasonUser() => await deepLinkService.createDeeplink(
+          ReplaceableEventReference(masterPubkey: reason.pubkey, kind: UserMetadataEntity.kind)
+              .encode(),
+        ),
+      ReportReasonContent() => await deepLinkService.createDeeplink(reason.eventReference.encode()),
+    };
+
     return switch (reason) {
-      ReportReasonUser() =>
-        'This is a report for the user ${ReplaceableEventReference(masterPubkey: reason.pubkey, kind: UserMetadataEntity.kind).encode()}',
-      ReportReasonContent() => 'This is a report for the content ${reason.eventReference.encode()}',
-      ReportReasonConversation() =>
-        'This is a report for the conversation ${reason.conversationId}',
+      ReportReasonUser() => 'This is a report for the user $encodedReason',
+      ReportReasonContent() => 'This is a report for the content $encodedReason',
     };
   }
 }
