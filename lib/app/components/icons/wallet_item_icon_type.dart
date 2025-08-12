e9d94c5068623c24346967231c68654f4e1f42f2
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-08-12 12:23:44 +0300

    chore: improve doc comments

--- lib/app/components/icons/wallet_item_icon_type.dart
+++ lib/app/components/icons/wallet_item_icon_type.dart
@@ -4,10 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:ion/app/extensions/extensions.dart';
 
 sealed class WalletItemIconType {
-  //  Custom size
+  /// Custom size. Please use it only if you really need it + don't use .s extension here,
+  /// since it will be converted to .s automatically.
   const factory WalletItemIconType.custom({required double size}) = _CustomSize;
 
-  // Predefined sizes
+  /// Predefined sizes
   factory WalletItemIconType.tiny() => const _PredefinedSize(12);
   factory WalletItemIconType.small() => const _PredefinedSize(16);
   factory WalletItemIconType.medium() => const _PredefinedSize(24);
@@ -18,8 +19,8 @@ sealed class WalletItemIconType {
 
   final double _size;
 
-  // Since network or coin icons may already have rounding as part of the image,
-  // we need to use dynamic borderRadius here to avoid image distortion.
+  /// Since network or coin icons may already have rounding as part of the image,
+  /// we need to use dynamic borderRadius here to avoid image distortion.
   BorderRadius get borderRadius => BorderRadius.circular(_size * 0.3);
 
   double get size => _size.s;
