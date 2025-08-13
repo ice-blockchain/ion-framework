811b4c37865c251b4a8528fc8527458fc07753d8
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-08-13 10:37:16 +0300

    chore: move button to the separate widget + create hook to calculate lines of text

--- lib/app/features/wallets/views/pages/info/info_modal.dart
+++ lib/app/features/wallets/views/pages/info/info_modal.dart
@@ -1,26 +1,43 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ion/app/components/card/info_card.dart';
 import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/wallets/model/info_type.dart';
+import 'package:ion/app/hooks/use_text_line_count.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_close_button.dart';
 
-class InfoModal extends StatelessWidget {
+class InfoModal extends HookWidget {
   const InfoModal({
     required this.infoType,
     super.key,
-    this.descriptionTextAlign,
   });
 
   final InfoType infoType;
-  final TextAlign? descriptionTextAlign;
 
   @override
   Widget build(BuildContext context) {
+    final description = infoType.getDesc(context);
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final availableWidth = screenWidth - (ScreenSideOffset.defaultMediumMargin * 2);
+
+    final descriptionStyle = context.theme.appTextThemes.body2.copyWith(
+      color: context.theme.appColors.secondaryText,
+    );
+
+    final lineCount = useTextLineCount(
+      text: description,
+      textStyle: descriptionStyle,
+      maxWidth: availableWidth,
+      scaler: MediaQuery.textScalerOf(context),
+    );
+
+    final textAlign = lineCount <= 3 ? TextAlign.center : TextAlign.start;
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -37,8 +54,8 @@ class InfoModal extends StatelessWidget {
           child: InfoCard(
             iconAsset: infoType.iconAsset,
             title: infoType.getTitle(context),
-            description: infoType.getDesc(context),
-            descriptionTextAlign: descriptionTextAlign ?? TextAlign.start,
+            description: description,
+            descriptionTextAlign: textAlign,
           ),
         ),
         ScreenBottomOffset(margin: 16.0.s),
---
+++ lib/app/hooks/use_text_line_count.dart
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+int useTextLineCount({
+  required String text,
+  required TextStyle textStyle,
+  required double maxWidth,
+  required TextScaler scaler,
+}) {
+  return useMemoized(
+    () {
+      if (text.isEmpty) return 0;
+
+      final textPainter = TextPainter(
+        text: TextSpan(text: text, style: textStyle),
+        textDirection: TextDirection.ltr,
+        textScaler: scaler,
+      )..layout(maxWidth: maxWidth);
+
+      final lineMetrics = textPainter.computeLineMetrics();
+      return lineMetrics.length;
+    },
+    [text, textStyle, maxWidth, scaler],
+  );
+}
