bc1a19e67681e9f6661399226b3a8b5579d1d64e
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-08-14 16:31:56 +0200

    fix: update local followers count after follow or unfollow (#1863)
    ## Description
    This PR fixes an issue where followers count was not updated on profile
    page after following/unfollowing
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Task ID
    ION-2466
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    
    
    https://github.com/user-attachments/assets/583695f8-4f6a-422c-a782-2e9eccd7f523

--- lib/app/features/user/providers/followers_count_provider.r.dart
+++ lib/app/features/user/providers/followers_count_provider.r.dart
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/feed/data/models/entities/event_count_request_data.f.dart';
 import 'package:ion/app/features/feed/data/models/entities/event_count_result_data.f.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
 import 'package:ion/app/features/ion_connect/model/action_source.f.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.r.dart';
 import 'package:ion/app/features/user/model/follow_list.f.dart';
 import 'package:ion/app/features/user/providers/count_provider.r.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -12,25 +12,57 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'followers_count_provider.r.g.dart';
 
 @riverpod
-FutureOr<int?> followersCount(
-  Ref ref,
-  String pubkey,
-) async {
-  final filters = [
-    RequestFilter(
-      kinds: const [FollowListEntity.kind],
-      tags: {
-        '#p': [pubkey],
-      },
-    ),
-  ];
+class FollowersCount extends _$FollowersCount {
+  @override
+  FutureOr<int?> build(String pubkey) async {
+    final filters = [
+      RequestFilter(
+        kinds: const [FollowListEntity.kind],
+        tags: {
+          '#p': [pubkey],
+        },
+      ),
+    ];
 
-  return await ref.watch(
-    countProvider(
-      actionSource: ActionSourceUser(pubkey),
-      requestData: EventCountRequestData(filters: filters),
+    return await ref.watch(
+      countProvider(
+        actionSource: ActionSourceUser(pubkey),
+        requestData: EventCountRequestData(filters: filters),
+        key: pubkey,
+        type: EventCountResultType.followers,
+      ).future,
+    ) as FutureOr<int?>;
+  }
+
+  void addOne() {
+    if (!state.hasValue) return;
+    final newValue = state.value! + 1;
+    state = AsyncValue.data(newValue);
+    _updateCache(newValue);
+  }
+
+  void removeOne() {
+    if (!state.hasValue) return;
+    final newValue = state.value! - 1;
+    state = AsyncValue.data(newValue);
+    _updateCache(newValue);
+  }
+
+  void _updateCache(int newCount) {
+    final cacheKey = EventCountResultEntity.cacheKeyBuilder(
       key: pubkey,
       type: EventCountResultType.followers,
-    ).future,
-  ) as FutureOr<int?>;
+    );
+
+    final cacheEntry =
+        ref.read(ionConnectCacheProvider.select(cacheSelector<EventCountResultEntity>(cacheKey)));
+
+    if (cacheEntry == null) {
+      return;
+    }
+
+    ref.read(ionConnectCacheProvider.notifier).cache(
+          cacheEntry.copyWith(data: cacheEntry.data.copyWith(content: newCount)),
+        );
+  }
 }
