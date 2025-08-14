91b7f86dfa7e0f92d12ee2419c3fd35c25011edb
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-08-14 12:47:52 +0200

    fix: update local followers count after follow or unfollow

--- lib/app/features/optimistic_ui/features/follow/follow_sync_strategy_provider.r.dart
+++ lib/app/features/optimistic_ui/features/follow/follow_sync_strategy_provider.r.dart
@@ -12,6 +12,7 @@ import 'package:ion/app/features/optimistic_ui/features/follow/follow_sync_strat
 import 'package:ion/app/features/optimistic_ui/features/follow/model/user_follow.f.dart';
 import 'package:ion/app/features/user/model/follow_list.f.dart';
 import 'package:ion/app/features/user/providers/follow_list_provider.r.dart';
+import 'package:ion/app/features/user/providers/followers_count_provider.r.dart';
 import 'package:ion/app/features/user/providers/user_events_metadata_provider.r.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -51,6 +52,7 @@ SyncStrategy<UserFollow> followSyncStrategy(Ref ref) {
           cache: false,
         ),
       ]);
+      ref.read(followersCountProvider(follow.pubkey).notifier).addOne();
     },
     deleteFollow: (follow) async {
       final followList = await ref.read(currentUserFollowListProvider.future);
@@ -95,6 +97,7 @@ SyncStrategy<UserFollow> followSyncStrategy(Ref ref) {
       final updatedFollowList = followList.data.copyWith(list: followees.toList());
       final updatedFollowEntity = followList.copyWith(data: updatedFollowList);
       ref.read(ionConnectCacheProvider.notifier).cache(updatedFollowEntity);
+      ref.read(followersCountProvider(pubkey).notifier).removeOne();
     },
   );
 }
