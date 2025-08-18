14acbcd288ce3ccf73e3ce7f152db49e4a287212
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-08-18 15:08:56 +0300

    fix: full screen video controller initialization errors

--- lib/app/hooks/use_auto_play.dart
+++ lib/app/hooks/use_auto_play.dart
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:video_player/video_player.dart';
 
 void useAutoPlay(VideoPlayerController? controller) {
   useEffect(
     () {
-      controller?.play();
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        controller?.play();
+      });
+
       return () {
-        controller?.pause();
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          controller?.pause();
+        });
       };
     },
     [controller],
