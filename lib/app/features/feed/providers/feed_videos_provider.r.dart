3d1b9bebc73514dcfa746782731b6ada36a2243a
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-08-18 21:52:21 +0300

    updating video page and provider

--- lib/app/features/feed/providers/feed_videos_provider.r.dart
+++ lib/app/features/feed/providers/feed_videos_provider.r.dart
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:async/async.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/features/feed/create_post/providers/create_post_notifier.m.dart';
 import 'package:ion/app/features/feed/data/models/feed_filter.dart';
 import 'package:ion/app/features/feed/data/models/feed_type.dart';
 import 'package:ion/app/features/feed/providers/feed_current_filter_provider.m.dart';
 import 'package:ion/app/features/feed/providers/feed_following_content_provider.m.dart';
 import 'package:ion/app/features/feed/providers/feed_for_you_content_provider.m.dart';
 import 'package:ion/app/features/feed/providers/feed_posts_provider.r.dart';
-import 'package:ion/app/features/feed/providers/repost_notifier.r.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/providers/entities_paged_data_provider.m.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -28,23 +24,21 @@ class FeedVideos extends _$FeedVideos with DelegatedPagedNotifier {
 
     final filter = ref.watch(feedCurrentFilterProvider.select((state) => state.filter));
 
-    // Try getting videos from posts provider instead
     final postsData = switch (filter) {
       FeedFilter.following => ref.watch(
-          feedFollowingContentProvider(FeedType.post)
+          feedFollowingContentProvider(FeedType.video)
               .select((data) => (items: data.items, hasMore: data.hasMore)),
         ),
       FeedFilter.forYou => ref.watch(
-          feedForYouContentProvider(FeedType.post)
+          feedForYouContentProvider(FeedType.video)
               .select((data) => (items: data.items, hasMore: data.hasMore)),
         ),
     };
-    
+    print('🔥 [d3g] FeedVideos build postsData: ${postsData.items?.length}');
     // Filter for videos only
-    final videoItems = postsData.items?.where((entity) => 
-      ref.read(isVideoPostProvider(entity)) || ref.read(isVideoRepostProvider(entity))
-    );
-    
+    final videoItems = postsData.items?.where((entity) =>
+        ref.read(isVideoPostProvider(entity)) || ref.read(isVideoRepostProvider(entity)));
+
     final data = (items: videoItems, hasMore: postsData.hasMore);
 
     // Trigger initial fetch if no data (commented out to prevent infinite loop)
@@ -52,20 +46,32 @@ class FeedVideos extends _$FeedVideos with DelegatedPagedNotifier {
     //   Future.microtask(fetchEntities);
     // }
 
-    print('🔥 [d3g] FeedVideos build filter: $filter');
-    print('🔥 [d3g] FeedVideos build data: ${data.items?.length}');
-    print('🔥 [d3g] FeedVideos build hasMore: ${data.hasMore}');
+    // print('🔥 [d3g] FeedVideos build filter: $filter');
+    // print('🔥 [d3g] FeedVideos build data: ${data.items?.length}');
+    // print('🔥 [d3g] FeedVideos build hasMore: ${data.hasMore}');
 
     return data;
   }
 
   @override
   Future<void> fetchEntities() async {
-    print('🔥 [d3g] FeedVideos fetchEntities called');
-    final result = await super.fetchEntities();
-    final currentData = ref.read(feedVideosProvider);
-    print('🔥 [d3g] FeedVideos fetchEntities completed, data length: ${currentData.items?.length}');
-    return result;
+    final filter = ref.read(feedCurrentFilterProvider.select((state) => state.filter));
+    // Try getting videos from posts provider instead
+    final notifier = feedForYouContentProvider(FeedType.video).notifier as FeedFollowingContent;
+    print('🔥 [d3g] FeedVideos fetchEntities');
+    await notifier.fetchEntities();
+    print('🔥 [d3g] FeedVideos fetchEntities completed');
+
+    // final contentProvider = switch (filter) {
+    //   FeedFilter.following => feedFollowingContentProvider(FeedType.video).notifier,
+    //   FeedFilter.forYou => feedForYouContentProvider(FeedType.video).notifier,
+    // };
+
+    // print('🔥 [d3g] FeedVideos fetchEntities called');
+    // final result = await super.fetchEntities();
+    // final currentData = ref.read(feedVideosProvider);
+    // print('🔥 [d3g] FeedVideos fetchEntities completed, data length: ${currentData.items?.length}');
+    // return result;
   }
 
   @override
--- lib/app/features/video/views/pages/feed_videos_page.dart
+++ lib/app/features/video/views/pages/feed_videos_page.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/feed/providers/feed_posts_provider.r.dart';
 import 'package:ion/app/features/feed/providers/feed_videos_provider.r.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.f.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/video/views/pages/videos_vertical_scroll_page.dart';
 
 class FeedVideosPage extends HookConsumerWidget {
@@ -19,9 +20,27 @@ class FeedVideosPage extends HookConsumerWidget {
   final EventReference? framedEventReference;
   final int initialMediaIndex;
 
+  Iterable<IonConnectEntity> _filterEntitiesFromEventReference(
+    Iterable<IonConnectEntity> entities,
+    EventReference targetEventReference,
+  ) {
+    final entitiesList = entities.toList();
+    final targetIndex = entitiesList.indexWhere(
+      (entity) => entity.toEventReference() == targetEventReference,
+    );
+    
+    if (targetIndex == -1) {
+      return entities;
+    }
+    
+    return entitiesList.skip(targetIndex);
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final entities = ref.watch(feedVideosProvider.select((state) => state.items ?? {}));
+    final allEntities = ref.watch(feedVideosProvider.select((state) => state.items ?? {}));
+
+    final entities = _filterEntitiesFromEventReference(allEntities, eventReference);
 
     final videoCount = entities
         .where(
@@ -36,6 +55,7 @@ class FeedVideosPage extends HookConsumerWidget {
       print('🔥 [d3g] FeedVideosPage fetchEntities');
       ref.read(feedVideosProvider.notifier).fetchEntities();
     }
+    print('🔥 [d3g] initialMediaIndex: ${initialMediaIndex}');
 
     return VideosVerticalScrollPage(
       eventReference: eventReference,
@@ -43,7 +63,10 @@ class FeedVideosPage extends HookConsumerWidget {
       framedEventReference: framedEventReference,
       hasMore: state.hasMore,
       entities: entities,
-      onLoadMore: () => ref.read(feedVideosProvider.notifier).fetchEntities(),
+      onLoadMore: () {
+        print('🔥 [d3g] FeedVideosPage onLoadMore');
+        ref.read(feedVideosProvider.notifier).fetchEntities();
+      },
     );
   }
 }
