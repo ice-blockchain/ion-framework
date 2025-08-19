9ff6bf4ff1146ea707cc8d4ce0f3d63bbba9256f
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-08-19 17:36:22 +0300

    fix: cancel compression and upload for media

--- lib/app/features/chat/e2ee/providers/send_chat_message/send_chat_media_provider.r.dart
+++ lib/app/features/chat/e2ee/providers/send_chat_message/send_chat_media_provider.r.dart
@@ -3,6 +3,8 @@
 import 'dart:async';
 
 import 'package:async/async.dart';
+import 'package:dio/dio.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_session.dart';
 import 'package:ion/app/features/chat/e2ee/providers/send_chat_message/compress_chat_media_provider.r.dart';
 import 'package:ion/app/features/chat/model/database/chat_database.m.dart';
 import 'package:ion/app/features/core/model/mime_type.dart';
@@ -15,6 +17,7 @@ import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.r.da
 import 'package:ion/app/features/ion_connect/providers/ion_connect_upload_notifier.m.dart';
 import 'package:ion/app/services/compressors/video_compressor.r.dart';
 import 'package:ion/app/services/ion_connect/ed25519_key_store.dart';
+import 'package:ion/app/services/logger/logger.dart';
 import 'package:ion/app/services/media_service/blurhash_service.r.dart';
 import 'package:ion/app/services/media_service/media_encryption_service.m.dart';
 import 'package:ion/app/services/media_service/media_service.m.dart';
@@ -24,6 +27,8 @@ part 'send_chat_media_provider.r.g.dart';
 
 @Riverpod(keepAlive: true)
 class SendChatMedia extends _$SendChatMedia {
+  final _cancelToken = CancelToken();
+  final Completer<FFmpegSession> _sessionIdCompleter = Completer<FFmpegSession>();
   CancelableOperation<AsyncValue<List<MediaAttachment>>>? _cancellableOperation;
 
   @override
@@ -33,8 +38,9 @@ class SendChatMedia extends _$SendChatMedia {
 
   Future<List<(String, List<MediaAttachment>)>> sendChatMedia(
     List<String> participantsMasterPubkeys,
-    MediaFile mediaFile,
-  ) async {
+    MediaFile mediaFile, {
+    CancelToken? cancelToken,
+  }) async {
     final mediaAttachments = <MediaAttachment>[];
     final result = <(String, List<MediaAttachment>)>[];
 
@@ -42,18 +48,28 @@ class SendChatMedia extends _$SendChatMedia {
 
     _cancellableOperation = CancelableOperation.fromFuture(
       AsyncValue.guard(() async {
+        Logger.log('Preparing to compress and upload media file: ${mediaFile.path}');
         final compressedMediaFile = await ref.read(
-          compressChatMediaProvider(mediaFile),
+          compressChatMediaProvider(
+            mediaFile,
+            sessionIdCompleter: _sessionIdCompleter,
+          ),
         );
+        Logger.log('Media file compressed: ${compressedMediaFile.path}');
 
         for (final participantKey in participantsMasterPubkeys) {
           if (_cancellableOperation?.isCanceled ?? false) {
             return [];
           }
 
+          Logger.log('Processing media for participant: $participantKey');
           final processedAttachments = await _processMedia(
             compressedMediaFile,
             participantKey,
+            cancelToken: _cancelToken,
+          );
+          Logger.log(
+            'Processed ${processedAttachments.length} media attachments for participant: $participantKey',
           );
 
           mediaAttachments.addAll(processedAttachments);
@@ -83,13 +99,17 @@ class SendChatMedia extends _$SendChatMedia {
 
   Future<void> cancel() async {
     await _cancellableOperation?.cancel();
+    _cancelToken.cancel('User cancelled upload');
     await ref.read(messageMediaDaoProvider).cancel(messageMediaId);
+    final compressionSessionId = await _sessionIdCompleter.future;
+    await compressionSessionId.cancel();
   }
 
   Future<List<MediaAttachment>> _processMedia(
     MediaFile mediaFile,
-    String masterPubkey,
-  ) async {
+    String masterPubkey, {
+    CancelToken? cancelToken,
+  }) async {
     final mediaAttachments = <MediaAttachment>[];
     final oneTimeEventSigner = await Ed25519KeyStore.generate();
     final env = ref.read(envProvider.notifier);
@@ -112,9 +132,10 @@ class SendChatMedia extends _$SendChatMedia {
         );
 
     final uploadResult = await ref.read(ionConnectUploadNotifierProvider.notifier).upload(
-          encryptedMediaFile.mediaFile,
           alt: FileAlt.message,
+          encryptedMediaFile.mediaFile,
           customEventSigner: oneTimeEventSigner,
+          cancelToken: cancelToken,
         );
 
     final isImage = mediaFile.mimeType == MimeType.image.value;
@@ -123,10 +144,7 @@ class SendChatMedia extends _$SendChatMedia {
     }
 
     final mediaMetadataEvent = await uploadResult.fileMetadata
-        .copyWith(
-      blurhash: blurHash,
-      thumb: thumbUrl,
-    )
+        .copyWith(blurhash: blurHash, thumb: thumbUrl)
         .toEventMessage(
       oneTimeEventSigner,
       tags: [
--- lib/app/services/compressors/compress_executor.r.dart
+++ lib/app/services/compressors/compress_executor.r.dart
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:async';
+
 import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:ffmpeg_kit_flutter/ffmpeg_session.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -11,9 +13,19 @@ part 'compress_executor.r.g.dart';
 class CompressExecutor {
   final _lock = Lock();
 
-  Future<FFmpegSession> execute(List<String> args) async {
+  Future<FFmpegSession> execute(
+    List<String> args,
+    Completer<FFmpegSession> sessionResultCompleter, {
+    Completer<FFmpegSession>? sessionIdCompleter,
+  }) async {
     return _lock.synchronized(() async {
-      return FFmpegKit.executeWithArguments(args);
+      final session = await FFmpegKit.executeWithArgumentsAsync(args, (sessionResult) {
+        sessionResultCompleter.complete(sessionResult);
+      });
+
+      sessionIdCompleter?.complete(session);
+
+      return sessionResultCompleter.future;
     });
   }
 }
