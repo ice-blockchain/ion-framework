57b9d63f58f0290c51f4a17175fca641748a0997
ice-priapus <227110521+ice-priapus@users.noreply.github.com>
2025-08-20 14:02:15 +0300

    fix: remove a condition on new collection button visibility in bookmarks

--- lib/app/features/user/pages/bookmarks_page/components/bookmarks_header.dart
+++ lib/app/features/user/pages/bookmarks_page/components/bookmarks_header.dart
@@ -1,16 +1,14 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/inputs/search_input/search_input.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/app/features/feed/providers/feed_bookmarks_notifier.r.dart';
 import 'package:ion/app/router/app_routes.gr.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/generated/assets.gen.dart';
 
-class BookmarksHeader extends HookConsumerWidget implements PreferredSizeWidget {
+class BookmarksHeader extends StatelessWidget implements PreferredSizeWidget {
   const BookmarksHeader({
     required this.onSearchQueryUpdated,
     required this.loading,
@@ -21,12 +19,7 @@ class BookmarksHeader extends HookConsumerWidget implements PreferredSizeWidget 
   final bool loading;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final collectionsAmount =
-        ref.watch(feedBookmarkCollectionsNotifierProvider).valueOrNull?.length ?? 0;
-    final hasCustomCollections =
-        collectionsAmount > 1; // there is always 1 default collection which cannot be edited
-
+  Widget build(BuildContext context) {
     return Column(
       children: [
         NavigationAppBar.screen(
@@ -34,20 +27,18 @@ class BookmarksHeader extends HookConsumerWidget implements PreferredSizeWidget 
             context.i18n.bookmarks_title,
             style: context.theme.appTextThemes.subtitle2,
           ),
-          actions: hasCustomCollections
-              ? [
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: Assets.svg.iconEditLink.icon(
-                      size: NavigationAppBar.actionButtonSide,
-                      color: context.theme.appColors.primaryText,
-                    ),
-                    onPressed: () {
-                      EditBookmarksRoute().push<void>(context);
-                    },
-                  ),
-                ]
-              : [],
+          actions: [
+            IconButton(
+              padding: EdgeInsets.zero,
+              icon: Assets.svg.iconEditLink.icon(
+                size: NavigationAppBar.actionButtonSide,
+                color: context.theme.appColors.primaryText,
+              ),
+              onPressed: () {
+                EditBookmarksRoute().push<void>(context);
+              },
+            ),
+          ],
         ),
         ScreenSideOffset.small(
           child: SearchInput(
