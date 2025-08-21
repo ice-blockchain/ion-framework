d3239b765d8f828d613dff94adce1376c6a6bcc5
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-08-21 18:10:39 +0300

    fix: unique services

--- lib/app/features/chat/views/components/message_items/message_reactions/optimistic_ui/message_reactions_provider.r.dart
+++ lib/app/features/chat/views/components/message_items/message_reactions/optimistic_ui/message_reactions_provider.r.dart
@@ -5,6 +5,7 @@ import 'package:ion/app/features/chat/model/database/chat_database.m.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_reactions/optimistic_ui/model/optimistic_message_reactions.f.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_reactions/optimistic_ui/reaction_sync_strategy_provider.r.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_reactions/optimistic_ui/toggle_reaction_intent.dart';
+import 'package:ion/app/features/core/providers/env_provider.r.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.f.dart';
 import 'package:ion/app/features/optimistic_ui/core/operation_manager.dart';
 import 'package:ion/app/features/optimistic_ui/core/optimistic_service.dart';
@@ -32,10 +33,12 @@ Stream<List<OptimisticMessageReactions>> subscribeToMessageReactions(
 }
 
 @riverpod
-OptimisticOperationManager<OptimisticMessageReactions> messageReactionManager(Ref ref) {
+OptimisticOperationManager<OptimisticMessageReactions> messageReactionManager(
+  Ref ref,
+  EventReference eventReference,
+) {
   final strategy = ref.watch(reactionSyncStrategyProvider);
-  final localEnabled = true;
-//  ref.watch(envProvider.notifier).get<bool>(EnvVariable.OPTIMISTIC_UI_ENABLED);
+  final localEnabled = ref.watch(envProvider.notifier).get<bool>(EnvVariable.OPTIMISTIC_UI_ENABLED);
 
   final manager = OptimisticOperationManager<OptimisticMessageReactions>(
     syncCallback: strategy.send,
@@ -49,19 +52,24 @@ OptimisticOperationManager<OptimisticMessageReactions> messageReactionManager(Re
 }
 
 @riverpod
-OptimisticService<OptimisticMessageReactions> messageReactionService(Ref ref) {
-  final manager = ref.watch(messageReactionManagerProvider);
+OptimisticService<OptimisticMessageReactions> messageReactionService(
+  Ref ref,
+  EventReference eventReference,
+) {
+  final manager = ref.watch(messageReactionManagerProvider(eventReference));
   final service = OptimisticService<OptimisticMessageReactions>(manager: manager);
 
   return service;
 }
 
 @riverpod
-Stream<OptimisticMessageReactions?> messageReactionWatch(Ref ref, EventReference eventReference) {
-  final service = ref.watch(messageReactionServiceProvider);
+Stream<OptimisticMessageReactions?> messageReactionWatch(
+  Ref ref,
+  EventReference eventReference,
+) {
+  final service = ref.watch(messageReactionServiceProvider(eventReference));
 
   final subscription = subscribeToMessageReactions(ref, eventReference).listen((s) {
-    print('Message reactions updated: ${s.first.reactions} reactions for $eventReference');
     service.initialize(s);
   });
 
@@ -80,7 +88,7 @@ class ToggleReactionNotifier extends _$ToggleReactionNotifier {
     required EventReference eventReference,
     required String currentUserMasterPubkey,
   }) async {
-    final service = ref.read(messageReactionServiceProvider);
+    final service = ref.read(messageReactionServiceProvider(eventReference));
 
     var current = ref.read(messageReactionWatchProvider(eventReference)).valueOrNull;
 
