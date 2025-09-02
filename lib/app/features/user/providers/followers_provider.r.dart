be6f259bf958c7aa8405d6ee2fef9c613a0b5d08
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-09-02 16:36:05 +0200

    fix: followers load more

--- lib/app/features/user/providers/followers_provider.r.dart
+++ lib/app/features/user/providers/followers_provider.r.dart
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/ion_connect/providers/entities_paged_data_provider.m.dart';
 import 'package:ion/app/features/user/model/user_metadata.f.dart';
 import 'package:ion/app/features/user/providers/followers_data_source_provider.r.dart';
@@ -10,25 +9,42 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'followers_provider.r.g.dart';
 
 @riverpod
-({bool hasMore, List<UserMetadataEntity>? users})? followers(
-  Ref ref, {
-  required String pubkey,
-  required String query,
-}) {
-  if (query.isNotEmpty) {
-    return ref
-        .watch(
-          searchUsersProvider(
-            query: query,
-            followerOfPubkey: pubkey,
-          ),
-        )
-        .valueOrNull;
+class Followers extends _$Followers {
+  @override
+  FutureOr<({bool hasMore, List<UserMetadataEntity>? users})?> build({
+    required String pubkey,
+    required String query,
+  }) async {
+    if (query.isNotEmpty) {
+      return ref
+          .watch(
+            searchUsersProvider(
+              query: query,
+              followerOfPubkey: pubkey,
+            ),
+          )
+          .valueOrNull;
+    }
+    final dataSource = ref.watch(followersDataSourceProvider(pubkey));
+    final entitiesPagedData = ref.watch(entitiesPagedDataProvider(dataSource));
+    return (
+      hasMore: entitiesPagedData?.hasMore ?? false,
+      users: entitiesPagedData?.data.items?.whereType<UserMetadataEntity>().toList(),
+    );
   }
-  final dataSource = ref.watch(followersDataSourceProvider(pubkey));
-  final entitiesPagedData = ref.watch(entitiesPagedDataProvider(dataSource));
-  return (
-    hasMore: entitiesPagedData?.hasMore ?? false,
-    users: entitiesPagedData?.data.items?.whereType<UserMetadataEntity>().toList(),
-  );
+
+  Future<void> loadMore() async {
+    if (query.isNotEmpty) {
+      return ref
+          .read(
+            searchUsersProvider(
+              query: query,
+              followerOfPubkey: pubkey,
+            ).notifier,
+          )
+          .loadMore();
+    }
+    final dataSource = ref.read(followersDataSourceProvider(pubkey));
+    await ref.read(entitiesPagedDataProvider(dataSource).notifier).fetchEntities();
+  }
 }
