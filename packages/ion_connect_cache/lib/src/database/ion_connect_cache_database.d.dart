37b4e33f800a3e63db61f4c372d1d41d9b544aa9
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-09-05 11:56:39 +0300

    refactor: migrate to cacheKey instead of event reference

--- packages/ion_connect_cache/lib/src/database/ion_connect_cache_database.d.dart
+++ packages/ion_connect_cache/lib/src/database/ion_connect_cache_database.d.dart
@@ -2,6 +2,7 @@
 
 import 'package:drift/drift.dart';
 import 'package:ion_connect_cache/src/database/converters/event_tags_converter.dart';
+import 'package:ion_connect_cache/src/database/ion_connect_cache_database.d.steps.dart';
 import 'package:ion_connect_cache/src/database/tables/event_messages_table.d.dart';
 
 part 'ion_connect_cache_database.d.g.dart';
@@ -11,5 +12,21 @@ class IONConnectCacheDatabase extends _$IONConnectCacheDatabase {
   IONConnectCacheDatabase(super.e);
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onCreate: (m) => m.createAll(),
+      onUpgrade: stepByStep(
+        from1To2: (m, schema) async {
+          await m.renameColumn(
+            eventMessagesTable,
+            'event_reference',
+            schema.eventMessagesTable.cacheKey,
+          );
+        },
+      ),
+    );
+  }
 }
