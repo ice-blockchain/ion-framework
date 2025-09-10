341e245bb405de60f508828bd531339f46103721
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-09-10 12:38:46 +0300

    feat: fix conflicts

--- lib/app/services/deep_link/deep_link_service.r.dart
+++ lib/app/services/deep_link/deep_link_service.r.dart
@@ -18,7 +18,6 @@ import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.
 import 'package:ion/app/features/ion_connect/model/event_reference.f.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_database_cache_notifier.r.dart';
-import 'package:ion/app/features/ion_connect/providers/ion_connect_db_cache_notifier.r.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_entity_provider.r.dart';
 import 'package:ion/app/features/user/model/user_metadata.f.dart';
 import 'package:ion/app/features/user/model/user_relays.f.dart';
