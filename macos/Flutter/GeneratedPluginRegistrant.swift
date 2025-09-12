df8821e0b35841c43a4826416ddabf6bd6a03e42
ion-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-09-12 15:42:51 +0200

    Revert "feat: migrate to Flutter-optimized cryptography implementation (#2038)"
    This reverts commit bee01f2eeedfeed12a5b245f5d317a4f210445fa.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import cryptography_flutter
 import device_info_plus
 import ffmpeg_kit_flutter
 import file_picker
@@ -37,7 +36,6 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
