261523ffe3b355f064a52e03345a9e5ef8e808ba
ice-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-09-15 11:41:21 +0200

    feat: add cryptography_flutter plugin (#2053)
    ## Description
    - this PR updates our cryptography setup by adding the
    [cryptography_flutter](https://pub.dev/packages/cryptography_flutter)
    package alongside the existing
    [cryptography](https://pub.dev/packages/cryptography) package, to
    achieve native performance.
    
    ## Additional Notes
    - add fork with fix for CI/CD
    
    ## Task ID
    ION-3861
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import cryptography_flutter
 import device_info_plus
 import ffmpeg_kit_flutter
 import file_picker
@@ -36,6 +37,7 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
