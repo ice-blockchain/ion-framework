f7597f1f89233052bcd3154aeb2b438b49e7b640
ion-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-09-24 12:52:00 +0200

    refactor: improve emoji detection using regex pattern matching in iOS extension

--- ios/NotificationServiceExtension/Translation/Extensions/String+Extensions.swift
+++ ios/NotificationServiceExtension/Translation/Extensions/String+Extensions.swift
@@ -3,16 +3,28 @@
 import Foundation
 
 extension String {
+    private static let emojiOnlyRegex: NSRegularExpression? = {
+        do {
+            // Match Flutter's regex pattern exactly
+            return try NSRegularExpression(
+                pattern: #"^(?:\p{Emoji}|\p{Emoji_Presentation}|\p{Extended_Pictographic})(?!\d)$"#,
+                options: []
+            )
+        } catch {
+            return nil
+        }
+    }()
+    
     var isEmoji: Bool {
         guard !isEmpty else { return false }
         
-        if CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: self)) {
+        if self.allSatisfy({ $0.isNumber }) {
             return false
         }
         
-        return unicodeScalars.allSatisfy { scalar in
-            scalar.properties.isEmoji || 
-            scalar.properties.isEmojiPresentation
-        }
+        guard let regex = Self.emojiOnlyRegex else { return false }
+        
+        let range = NSRange(location: 0, length: self.utf16.count)
+        return regex.firstMatch(in: self, options: [], range: range) != nil
     }
 }
