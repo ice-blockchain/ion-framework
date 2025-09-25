eb84b269701d75a6d7a8aca08bbc92a4bb1c1645
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-09-25 16:00:38 +0300

    chore: use provider for the comparator

--- lib/app/features/wallets/providers/networks_provider.r.dart
+++ lib/app/features/wallets/providers/networks_provider.r.dart
@@ -3,7 +3,7 @@
 import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/wallets/data/repository/networks_repository.r.dart';
-import 'package:ion/app/features/wallets/domain/networks/networks_comparator.dart';
+import 'package:ion/app/features/wallets/domain/networks/networks_comparator.r.dart';
 import 'package:ion/app/features/wallets/model/network_data.f.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -26,11 +26,11 @@ Future<List<NetworkData>> networksByTier(Ref ref, {required int tier}) {
 
 @riverpod
 Future<List<NetworkData>> networksWithNft(Ref ref) {
-  final comparator = NetworksComparator(sortType: NetworkSortType.nftPopularity);
+  final comparator = ref.watch(networksComparatorProvider);
   return ref
       .watch(networksRepositoryProvider)
       .getNftNetworks()
-      .then((networks) => networks.sorted(comparator.compareNetworks));
+      .then((networks) => networks.sorted(comparator.compareNftNetworksByPopularity));
 }
 
 @riverpod
