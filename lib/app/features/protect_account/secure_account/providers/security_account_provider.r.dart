62167c42b06335f0df8dda1adfb6e6d2b924d8e6
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-09-26 13:13:48 +0300

    fix: soft revert backup checkmarks logic

--- lib/app/features/protect_account/secure_account/providers/security_account_provider.r.dart
+++ lib/app/features/protect_account/secure_account/providers/security_account_provider.r.dart
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/features/protect_account/backup/providers/cloud_stored_recovery_keys_names_provider.r.dart';
 import 'package:ion/app/features/protect_account/secure_account/data/models/security_methods.f.dart';
 import 'package:ion/app/features/protect_account/secure_account/providers/recovery_credentials_enabled_notifier.r.dart';
-import 'package:ion/app/features/protect_account/secure_account/providers/recovery_keys_completed_provider.r.dart';
 import 'package:ion/app/features/protect_account/secure_account/providers/user_details_provider.r.dart';
 import 'package:ion/app/utils/predicates.dart';
 import 'package:ion_identity_client/ion_identity.dart';
@@ -14,12 +12,9 @@ part 'security_account_provider.r.g.dart';
 
 @Riverpod(keepAlive: true)
 Future<SecurityMethods> securityAccountController(Ref ref) async {
-  final (userDetails, recoveryCredentialsEnabled, hasCurrentUserBackup, recoveryKeysCompleted) =
-      await (
+  final (userDetails, recoveryCredentialsEnabled) = await (
     ref.watch(userDetailsProvider.future),
     ref.watch(recoveryCredentialsEnabledProvider.future),
-    ref.watch(hasCurrentUserBackupInCloudProvider.future),
-    ref.watch(recoveryKeysCompletedProvider.future),
   ).wait;
   final twoFaOptions = userDetails.twoFaOptions ?? [];
 
@@ -27,9 +22,7 @@ Future<SecurityMethods> securityAccountController(Ref ref) async {
     isEmailEnabled: twoFaOptions.any(startsWith(const TwoFAType.email().option)),
     isPhoneEnabled: twoFaOptions.any(startsWith(const TwoFAType.sms().option)),
     isAuthenticatorEnabled: twoFaOptions.any(startsWith(const TwoFAType.authenticator().option)),
-    isBackupEnabled: recoveryCredentialsEnabled && (hasCurrentUserBackup || recoveryKeysCompleted),
-    hasCloudEnabled: hasCurrentUserBackup,
-    hasRecoveryKeysEnabled: recoveryKeysCompleted,
+    isBackupEnabled: recoveryCredentialsEnabled,
   );
 }
 
