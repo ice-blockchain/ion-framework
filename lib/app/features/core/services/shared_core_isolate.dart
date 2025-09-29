8a62b0eeceac1763f6159e6fc696c12731dfbb15
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-09-29 21:24:05 +0300

    docs: add comments to clarify the purpose of sharedCoreIsolate for offloading tasks

--- lib/app/features/core/services/shared_core_isolate.dart
+++ lib/app/features/core/services/shared_core_isolate.dart
@@ -3,8 +3,12 @@
 import 'package:ion/app/services/ion_connect/ion_connect_signature_verifier.dart';
 import 'package:isolate_manager/isolate_manager.dart';
 
+///
+/// It is used to offload work from the main isolate.
+/// Prevents spawning multiple isolates for redundant tasks.
+/// Use this to efficiently handle background tasks without blocking the main isolate.
+///
 final sharedCoreIsolate = IsolateManager.createShared(
-  isDebug: true,
   workerMappings: {
     ionConnectSignatureVerifierFn: 'ionConnectSignatureVerifierFn',
   },
