b876d14f20c7349d38a9729fcfb081a5886047d3
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-09-29 15:59:59 +0200

    fix: remove scoll in ToolbarLinkButton ios

--- lib/app/features/feed/views/components/toolbar_buttons/toolbar_link_button.dart
+++ lib/app/features/feed/views/components/toolbar_buttons/toolbar_link_button.dart
@@ -20,8 +20,7 @@ class ToolbarLinkButton extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final styleManager =
-        useMemoized(() => QuillStyleManager(textEditorController), [textEditorController]);
+    final styleManager = useMemoized(() => QuillStyleManager(textEditorController), [textEditorController]);
 
     final selectionState = useState<TextSelection>(textEditorController.selection);
     useEffect(
@@ -37,11 +36,8 @@ class ToolbarLinkButton extends HookWidget {
     );
     final selection = selectionState.value;
 
-    final hasNonWhitespaceSelection = textEditorController.document
-        .toPlainText()
-        .substring(selection.start, selection.end)
-        .trim()
-        .isNotEmpty;
+    final hasNonWhitespaceSelection =
+        textEditorController.document.toPlainText().substring(selection.start, selection.end).trim().isNotEmpty;
 
     return ActionsToolbarButton(
       icon: Assets.svg.iconArticleLink,
@@ -49,8 +45,7 @@ class ToolbarLinkButton extends HookWidget {
       onPressed: () async {
         final localContext = context;
         String? existingLink;
-        final linkAttribute =
-            textEditorController.getSelectionStyle().attributes[Attribute.link.key];
+        final linkAttribute = textEditorController.getSelectionStyle().attributes[Attribute.link.key];
         if (linkAttribute != null) {
           existingLink = linkAttribute.value as String?;
         }
@@ -87,24 +82,27 @@ class ToolbarLinkButton extends HookWidget {
         });
         return CupertinoAlertDialog(
           title: Text(dialogContext.i18n.toolbar_link_title),
-          content: Column(
-            children: [
-              const SizedBox(height: 10),
-              CupertinoTextField(
-                controller: linkController,
-                focusNode: linkFocusNode,
-                placeholder: dialogContext.i18n.toolbar_link_placeholder,
-                placeholderStyle: dialogContext.theme.appTextThemes.body2.copyWith(
-                  color: dialogContext.theme.appColors.tertiaryText,
+          content: SizedBox(
+            height: 44,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                CupertinoTextField(
+                  controller: linkController,
+                  focusNode: linkFocusNode,
+                  placeholder: dialogContext.i18n.toolbar_link_placeholder,
+                  placeholderStyle: dialogContext.theme.appTextThemes.body2.copyWith(
+                    color: dialogContext.theme.appColors.tertiaryText,
+                  ),
+                  style: dialogContext.theme.appTextThemes.body2.copyWith(
+                    color: dialogContext.theme.appColors.primaryText,
+                  ),
+                  inputFormatters: [
+                    emojiRestrictionFormatter(),
+                  ],
                 ),
-                style: dialogContext.theme.appTextThemes.body2.copyWith(
-                  color: dialogContext.theme.appColors.primaryText,
-                ),
-                inputFormatters: [
-                  emojiRestrictionFormatter(),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
           actions: [
             CupertinoDialogAction(
