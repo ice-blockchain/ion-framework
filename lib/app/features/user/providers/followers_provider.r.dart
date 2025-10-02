8cc21545f62d488d36e258a7447c1a965171fcd7
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-10-02 10:02:25 +0200

    fix: improve loading in followers list by awaiting all missing events (#2192)
    ## Description
    This PR improves the loading and data appearance on followers list by
    awaiting all missing events fetch.
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Task ID
    3964
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    Before:
    
    https://github.com/user-attachments/assets/99c7f451-2c9e-49dc-a673-2b1da6a143e0
    
    
    After:
    
    https://github.com/user-attachments/assets/ea4f1f1e-458e-4b14-810b-47a717604bb8

--- lib/app/features/user/providers/followers_provider.r.dart
+++ lib/app/features/user/providers/followers_provider.r.dart
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:ion/app/features/core/model/paged.f.dart';
 import 'package:ion/app/features/ion_connect/providers/entities_paged_data_provider.m.dart';
 import 'package:ion/app/features/user/model/user_metadata.f.dart';
 import 'package:ion/app/features/user/providers/followers_data_source_provider.r.dart';
@@ -11,12 +12,12 @@ part 'followers_provider.r.g.dart';
 @riverpod
 class Followers extends _$Followers {
   @override
-  FutureOr<({bool hasMore, List<UserMetadataEntity>? users})?> build({
+  FutureOr<({bool hasMore, List<UserMetadataEntity>? users, bool ready})?> build({
     required String pubkey,
     required String query,
   }) async {
     if (query.isNotEmpty) {
-      return ref
+      final result = ref
           .watch(
             searchUsersProvider(
               query: query,
@@ -24,13 +25,26 @@ class Followers extends _$Followers {
             ),
           )
           .valueOrNull;
+      return (
+        hasMore: result?.hasMore ?? false,
+        users: result?.users,
+        ready: true,
+      );
     }
     final dataSource = ref.watch(followersDataSourceProvider(pubkey));
-    final entitiesPagedData = ref.watch(entitiesPagedDataProvider(dataSource));
-    return (
-      hasMore: entitiesPagedData?.hasMore ?? false,
-      users: entitiesPagedData?.data.items?.whereType<UserMetadataEntity>().toList(),
+    final entitiesPagedData = ref.watch(
+      entitiesPagedDataProvider(
+        dataSource,
+        awaitMissingEvents: true,
+      ),
     );
+    final users = entitiesPagedData?.data.items?.whereType<UserMetadataEntity>().toList();
+    final response = (
+      hasMore: entitiesPagedData?.hasMore ?? false,
+      users: users,
+      ready: (users?.length ?? 0) >= 12 || entitiesPagedData?.data is! PagedLoading,
+    );
+    return response;
   }
 
   Future<void> loadMore() async {
