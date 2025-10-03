99afbdf406cfb8202fdf7787b5a52e00ef4f53db
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-10-03 15:26:12 +0300

    test: process by 5 events in queue

--- lib/app/features/ion_connect/providers/global_subscription_event_dispatcher_provider.r.dart
+++ lib/app/features/ion_connect/providers/global_subscription_event_dispatcher_provider.r.dart
@@ -22,13 +22,37 @@ class GlobalSubscriptionEventDispatcher {
 
   final Ref ref;
   final List<GlobalSubscriptionEventHandler?> _handlers;
+  final List<EventMessage> _eventQueue = [];
+  bool _isProcessing = false;
 
   void dispatch(EventMessage eventMessage) {
-    final futures = _handlers.nonNulls
-        .where((handler) => handler.canHandle(eventMessage))
-        .map((handler) => handler.handle(eventMessage));
+    _eventQueue.add(eventMessage);
+    _processQueue();
+  }
 
-    unawaited(Future.wait(futures));
+  Future<void> _processQueue() async {
+    if (_isProcessing) return;
+    _isProcessing = true;
+
+    try {
+      while (_eventQueue.isNotEmpty) {
+        // Take up to 5 events from the queue
+        final batchSize = _eventQueue.length > 5 ? 5 : _eventQueue.length;
+        final batch = _eventQueue.sublist(0, batchSize);
+        _eventQueue.removeRange(0, batchSize);
+
+        // Process the batch sequentially
+        for (final eventMessage in batch) {
+          final futures = _handlers.nonNulls
+              .where((handler) => handler.canHandle(eventMessage))
+              .map((handler) => handler.handle(eventMessage));
+
+          await Future.wait(futures);
+        }
+      }
+    } finally {
+      _isProcessing = false;
+    }
   }
 }
 
