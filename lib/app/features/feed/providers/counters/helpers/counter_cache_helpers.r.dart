8377d3410f1dc7220a427ff0789274694bebf6d1
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-10-07 13:31:20 +0300

    cleanup

--- lib/app/features/feed/providers/counters/helpers/counter_cache_helpers.r.dart
+++ lib/app/features/feed/providers/counters/helpers/counter_cache_helpers.r.dart
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/feed/data/models/entities/event_count_result_data.f.dart';
 import 'package:ion/app/features/feed/reposts/models/post_repost.f.dart';
 import 'package:ion/app/features/feed/reposts/providers/optimistic/intents/add_quote_intent.dart';
