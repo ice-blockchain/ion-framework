8d5f2c4c9942c97a7720a835a1c1e0862abd44ea
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-10-07 10:42:58 +0300

    Revert "fix: fix issue of double splash when opening from push (#2125)"
    This reverts commit a0151e6a0d2e2a25bdf12deb88afdb24f925b774.

--- lib/app/features/push_notifications/providers/notification_response_handler_provider.r.dart
+++ lib/app/features/push_notifications/providers/notification_response_handler_provider.r.dart
@@ -2,8 +2,6 @@
 
 import 'dart:async';
 
-import 'package:ion/app/features/core/providers/init_provider.r.dart';
-import 'package:ion/app/features/core/providers/splash_provider.r.dart';
 import 'package:ion/app/features/push_notifications/providers/configure_firebase_app_provider.r.dart';
 import 'package:ion/app/features/push_notifications/providers/notification_response_service.r.dart';
 import 'package:ion/app/services/firebase/firebase_messaging_service_provider.r.dart';
@@ -44,7 +42,7 @@ class NotificationResponseHandler extends _$NotificationResponseHandler {
     // Notifications are handled there with a Notification Service Extension then passed to FCM SDK.
     final initialFcmNotificationData = await firebaseMessagingService.getInitialMessageData();
     if (initialFcmNotificationData != null) {
-      _handleInitialPushData(initialFcmNotificationData);
+      _handlePushData(initialFcmNotificationData);
     }
 
     // When the app is opened from a terminated state by a notification.
@@ -53,7 +51,7 @@ class NotificationResponseHandler extends _$NotificationResponseHandler {
     final initialLocalNotificationData =
         await localNotificationsService.getInitialNotificationData();
     if (initialLocalNotificationData != null) {
-      _handleInitialPushData(initialLocalNotificationData);
+      _handlePushData(initialLocalNotificationData);
     }
 
     // if the app is opened from a background state (not terminated) by pressing an FCM notification.
@@ -67,20 +65,5 @@ class NotificationResponseHandler extends _$NotificationResponseHandler {
 
   void _handlePushData(Map<String, dynamic> data) {
     ref.read(notificationResponseServiceProvider).handleNotificationResponse(data);
-  }
-
-  void _handleInitialPushData(Map<String, dynamic> data) {
-    // Wait for splash animation to complete before handling push notification
-    final subscription = ref.listen(splashProvider, (prev, animationCompleted) async {
-      if (animationCompleted) {
-        final isInitCompleted = ref.read(initAppProvider).hasValue;
-        if (!isInitCompleted) {
-          await ref.read(initAppProvider.future);
-        }
-
-        unawaited(ref.read(notificationResponseServiceProvider).handleNotificationResponse(data));
-      }
-    });
-    ref.onDispose(subscription.close);
   }
 }
