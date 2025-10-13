cbb1a95cf05a685c31e9b8e625a6bc661e88c4b1
ice-phobos <236537994+ice-phobos@users.noreply.github.com>
2025-10-13 13:00:10 +0400

    chore: logs added to get is_nip_96 required in file_storage

--- lib/app/features/ion_connect/utils/file_storage_utils.dart
+++ lib/app/features/ion_connect/utils/file_storage_utils.dart
@@ -12,6 +12,7 @@ import 'package:ion/app/features/ion_connect/model/file_storage_metadata.f.dart'
 import 'package:ion/app/features/ion_connect/model/ion_connect_auth.f.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.r.dart';
 import 'package:ion/app/features/user/providers/relays/ranked_user_relays_provider.r.dart';
+import 'package:ion/app/services/logger/logger.dart';
 
 Future<String> generateAuthorizationToken({
   required Ref ref,
@@ -53,14 +54,28 @@ Future<String> getFileStorageApiUrl(
       path: FileStorageMetadata.path,
     );
 
+    // NOSTR.NIP96 get_start
+    final discoveryHost = metadataUri.authority;
+    Logger.info('NOSTR.NIP96 get_start host=$discoveryHost');
+
     final response = await ref.read(dioProvider).getUri<dynamic>(
           metadataUri,
           cancelToken: cancelToken,
         );
-    final path = FileStorageMetadata.fromJson(
-      json.decode(response.data as String) as Map<String, dynamic>,
-    ).apiUrl;
-    return metadataUri.replace(path: path).toString();
+    final jsonMap = json.decode(response.data as String) as Map<String, dynamic>;
+    final metadata = FileStorageMetadata.fromJson(jsonMap);
+    final path = metadata.apiUrl;
+    final uploadUrl = metadataUri.replace(path: path).toString();
+
+    // Parse is_nip98_required from top-level of or plans from jsonMap (For log purposes)
+    final isNip98Required =
+        (jsonMap['is_nip98_required'] as bool?) ?? _extractIsNip98FromPlans(jsonMap['plans']);
+
+    // NOSTR.NIP96 get_ok
+    Logger.info(
+        'NOSTR.NIP96 get_ok host=$discoveryHost upload_url=$uploadUrl is_nip98_required=$isNip98Required');
+
+    return uploadUrl;
   } catch (error) {
     if (_isRelayDead(error)) {
       ref.read(rankedCurrentUserRelaysProvider.notifier).reportUnreachableRelay(relayUrl);
@@ -76,4 +91,20 @@ Future<String> getFileStorageApiUrl(
 bool _isRelayDead(Object error) {
   // To be adjusted
   return true;
+}
+
+/// Extracts is_nip98_required from plans in jsonMap (For log purposes only)
+/// We need this when is_nip98_required is not present at the top-level of the jsonMap
+bool _extractIsNip98FromPlans(dynamic plansJson) {
+  if (plansJson is Map<String, dynamic>) {
+    for (final entry in plansJson.values) {
+      if (entry is Map<String, dynamic>) {
+        final value = entry['is_nip98_required'];
+        if (value is bool && value) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
