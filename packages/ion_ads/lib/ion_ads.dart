9070a76312e3c05dfa9a7164bfc5d94554323f22
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-10-14 18:39:16 +0300

    feat: adding ads module

---
+++ packages/ion_ads/lib/ion_ads.dart
@@ -0,0 +1,13 @@
+library ion_ads;
+
+export 'src/models/ad_types.dart';
+export 'src/models/native_ad_asset.dart';
+export 'src/services/ads_platform.dart';
+export 'src/services/mock_ads_platform.dart';
+export 'src/services/ad_insertion_helper.dart';
+export 'src/widgets/native_ad_card.dart';
+export 'src/widgets/native_story_ad.dart';
+export 'src/widgets/native_video_ad.dart';
+export 'src/widgets/native_chat_ad.dart';
+export 'src/widgets/native_article_ad.dart';
+export 'src/widgets/native_chat_list_ad.dart';
