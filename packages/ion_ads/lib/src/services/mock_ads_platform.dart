818a352e3b92608740043f9aa852e9f284ac07b4
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-10-14 18:39:16 +0300

    feat: adding ads module

---
+++ packages/ion_ads/lib/src/services/mock_ads_platform.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+
+import '../models/ad_types.dart';
+import '../models/native_ad_asset.dart';
+import 'ads_platform.dart';
+
+class MockIonAdsPlatform implements IonAdsPlatform {
+  @override
+  Future<void> initialize({required String appKey}) async {}
+
+  @override
+  Future<bool> isAvailable(IonNativeAdPlacement placement) async => true;
+
+  @override
+  Future<IonNativeAdAsset?> loadNativeAd({
+    required IonNativeAdPlacement placement,
+    Map<String, Object?>? targeting,
+  }) async {
+    await Future<void>.delayed(const Duration(milliseconds: 200));
+    return IonNativeAdAsset(
+      title: 'Sponsored by Acme',
+      body: 'Discover the new Acme Widget that boosts your productivity.',
+      callToAction: 'Learn more',
+      iconImage: null,
+      mediaContent: null,
+      rating: 4.6,
+      advertiser: 'Acme Inc.',
+      attributionText: 'Ad',
+    );
+  }
+
+  @override
+  Widget? buildPlatformMediaView(IonNativeAdAsset adAsset) => null;
+}
