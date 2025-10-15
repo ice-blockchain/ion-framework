ad3924970bdf508a08ed13ed66ece1570f6bf9da
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-10-15 12:03:50 +0300

    chore: add btc exception handler

---
+++ lib/app/features/wallets/domain/transactions/transfer_exception_handlers/bitcoin_exception_handler.dart
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/features/wallets/domain/transactions/transfer_exception_handlers/transfer_exception_handler.dart';
+import 'package:ion/app/features/wallets/model/coin_data.f.dart';
+
+class BitcoinExceptionHandler implements TransferExceptionHandler {
+  @override
+  IONException? tryHandle(String? reason, CoinData coin) {
+    if (!coin.network.isBitcoin) return null;
+
+    if (reason != null) {
+      if (reason.toLowerCase().contains('dust')) {
+        return InsufficientAmountException();
+      }
+    }
+
+    return null;
+  }
+}
