6be2ce5f1189f8f31d61677c7dd43bc6c9f64743
ice-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-10-15 09:08:55 +0200

    feat: format count (#2305)
    ## Description
    - add number formatting with K/M suffix for large counts
    
    ## Task ID
    ION-4168
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- test/utils/num_test.dart
+++ test/utils/num_test.dart
@@ -6,6 +6,27 @@ import 'package:ion/app/utils/num.dart';
 import '../test_utils.dart';
 
 void main() {
+  parameterizedGroup('formatCount', [
+    (value: 9999, expected: '9,999'),
+    (value: 10000, expected: '10K'),
+    (value: 10120, expected: '10.1K'),
+    (value: 12345, expected: '12.3K'),
+    (value: 99999, expected: '100K'),
+    (value: 999999, expected: '1M'),
+    (value: 1000000, expected: '1M'),
+    (value: 1234567, expected: '1.23M'),
+    (value: 12345678, expected: '12.3M'),
+    (value: 500, expected: '500'),
+    (value: 0, expected: '0'),
+  ], (t) {
+    test(
+      'formatCount(${t.value})',
+      () {
+        final result = formatCount(t.value);
+        expect(result, t.expected);
+      },
+    );
+  });
   parameterizedGroup('formatted double value with default params', [
     (value: 1.0, expected: '1.00'),
     (value: 1.01, expected: '1.01'),
