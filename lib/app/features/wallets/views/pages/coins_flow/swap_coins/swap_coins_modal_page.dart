92caedff4743cb7418a302d8c1dab47733aa3a5e
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-10-17 12:38:34 +0200

    feat: implement Swap Coins modal enhancements with new UI components and SVG icon

--- lib/app/features/wallets/views/pages/coins_flow/swap_coins/swap_coins_modal_page.dart
+++ lib/app/features/wallets/views/pages/coins_flow/swap_coins/swap_coins_modal_page.dart
@@ -7,10 +7,13 @@ import 'package:ion/app/components/button/button.dart';
 import 'package:ion/app/components/icons/coin_icon.dart';
 import 'package:ion/app/components/icons/wallet_item_icon_type.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/wallets/model/coin_in_wallet_data.f.dart';
 import 'package:ion/app/features/wallets/model/coins_group.f.dart';
 import 'package:ion/app/features/wallets/providers/wallet_view_data_provider.r.dart';
+import 'package:ion/app/features/wallets/views/components/coin_icon_with_network.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/app/router/components/sheet_content/sheet_content.dart';
+import 'package:ion/app/utils/text_input_formatters.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class SwapCoinsModalPage extends ConsumerWidget {
@@ -25,6 +28,7 @@ class SwapCoinsModalPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final walletView = ref.watch(currentWalletViewDataProvider).requireValue;
     final initialCoins = walletView.coinGroups.firstWhere((e) => e.symbolGroup == initialCoinsGroupSymbol);
+    final initialCoin = initialCoins.coins.first;
 
     return SheetContent(
       body: Column(
@@ -44,6 +48,7 @@ class SwapCoinsModalPage extends ConsumerWidget {
           _TokenCard(
             type: _TokenCardType.from,
             coinsGroup: initialCoins,
+            coin: initialCoin,
           ),
           SizedBox(
             height: 10.0.s,
@@ -51,7 +56,16 @@ class SwapCoinsModalPage extends ConsumerWidget {
           const _TokenCard(
             type: _TokenCardType.to,
             coinsGroup: null,
+            coin: null,
           ),
+          SizedBox(
+            height: 16.0.s,
+          ),
+          const _SwapButton(),
+          SizedBox(
+            height: 16.0.s,
+          ),
+          const _ContinueButton(),
         ],
       ),
     );
@@ -102,10 +116,12 @@ enum _TokenCardType {
 class _TokenCard extends ConsumerWidget {
   const _TokenCard({
     required this.type,
+    required this.coin,
     required this.coinsGroup,
   });
 
   final _TokenCardType type;
+  final CoinInWalletData? coin;
   final CoinsGroup? coinsGroup;
 
   @override
@@ -182,9 +198,42 @@ class _TokenCard extends ConsumerWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               if (iconUrl != null && coinsGroup != null)
-                CoinIconWidget(
-                  imageUrl: iconUrl,
-                  type: WalletItemIconType.big(),
+                Row(
+                  spacing: 10.0.s,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (coin != null)
+                      CoinIconWithNetwork.small(
+                        iconUrl,
+                        network: coin!.coin.network,
+                      ),
+                    Column(
+                      spacing: 2.0.s,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          coinsGroup!.name,
+                          style: textStyles.body.copyWith(
+                            color: colors.primaryText,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 6.0.s, vertical: 2.0.s),
+                          decoration: BoxDecoration(
+                            color: colors.attentionBlock,
+                            borderRadius: BorderRadius.circular(16.0.s),
+                          ),
+                          child: Text(
+                            coin?.coin.network.displayName ?? '',
+                            style: textStyles.caption3.copyWith(
+                              color: colors.quaternaryText,
+                              fontSize: 11.0.s,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 )
               else
                 Button(
@@ -213,11 +262,56 @@ class _TokenCard extends ConsumerWidget {
                     ),
                   ),
                 ),
-              TextField(
-                readOnly: coinsGroup == null,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
+              SizedBox(
+                width: 150.0.s,
+                child: TextField(
+                  readOnly: coinsGroup == null,
+                  keyboardType: TextInputType.number,
+                  style: textStyles.headline2.copyWith(
+                    color: colors.primaryText,
+                  ),
+                  inputFormatters: [
+                    CoinInputFormatter(),
+                  ],
+                  textAlign: TextAlign.end,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: '0.00',
+                    hintStyle: textStyles.headline2.copyWith(
+                      color: colors.tertiaryText,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 8.0.s,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Assets.svg.iconWallet.icon(
+                    color: colors.tertiaryText,
+                    size: 12.0.s,
+                  ),
+                  SizedBox(
+                    width: 4.0.s,
+                  ),
+                  Text(
+                    coinsGroup != null ? '${coinsGroup!.totalAmount} ${coinsGroup!.symbolGroup}' : '0.00 ICE',
+                    style: textStyles.caption2.copyWith(
+                      color: colors.tertiaryText,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                r'$0.00',
+                style: textStyles.caption2.copyWith(
+                  color: colors.tertiaryText,
                 ),
               ),
             ],
@@ -240,7 +334,6 @@ class _SumPercentageAction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = context.theme.appColors;
-
     final textStyles = context.theme.appTextThemes;
 
     return GestureDetector(
@@ -257,6 +350,77 @@ class _SumPercentageAction extends StatelessWidget {
           '$percentage%',
           style: textStyles.caption3.copyWith(
             color: colors.quaternaryText,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SwapButton extends StatelessWidget {
+  const _SwapButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.theme.appColors;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.0.s),
+      child: Center(
+        child: Container(
+          width: 34.0.s,
+          height: 34.0.s,
+          decoration: BoxDecoration(
+            color: colors.tertiaryBackground,
+            borderRadius: BorderRadius.circular(12.0.s),
+            border: Border.all(
+              color: colors.secondaryBackground,
+              width: 3,
+            ),
+          ),
+          child: Center(
+            child: Assets.svg.iconamoonSwap.icon(
+              color: colors.primaryText,
+              size: 24.0.s,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ContinueButton extends StatelessWidget {
+  const _ContinueButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.theme.appColors;
+    final textStyles = context.theme.appTextThemes;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.0.s),
+      child: Button(
+        onPressed: () {
+          // TODO(ice-erebus): implement continue action
+        },
+        label: Text(
+          context.i18n.wallet_swap_coins_continue,
+          style: textStyles.body.copyWith(
+            color: colors.secondaryBackground,
+          ),
+        ),
+        backgroundColor: colors.sheetLine,
+        borderRadius: BorderRadius.circular(16.0.s),
+        trailingIcon: Assets.svg.iconButtonNext.icon(
+          color: colors.secondaryBackground,
+          size: 24.0.s,
+        ),
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 56.0.s),
+          padding: EdgeInsets.symmetric(
+            horizontal: 109.0.s,
+            vertical: 16.0.s,
           ),
         ),
       ),
