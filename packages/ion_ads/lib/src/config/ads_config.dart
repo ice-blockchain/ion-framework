74f739b10aac4340b290be457f3bf80dc1d0fec2
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-10-17 11:41:25 +0300

    [wip] example app

---
+++ packages/ion_ads/lib/src/config/ads_config.dart
@@ -0,0 +1,7 @@
+class AdsConfig {
+  // Provided iOS Appodeal app key
+  static const String iosAppodealAppKey = '088d84360f7649f76befc2b9277e00394dbc50552812ef3c';
+
+  // TODO: Provide Android Appodeal app key
+  static const String androidAppodealAppKey = '';
+}
