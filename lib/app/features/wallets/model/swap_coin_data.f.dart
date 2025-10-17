aba7de1f31f418eea210c24d077101e78df534f1
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-10-17 17:49:26 +0200

    chore: add SPDX license identifier to message notification state and swap coin data files

--- lib/app/features/wallets/model/swap_coin_data.f.dart
+++ lib/app/features/wallets/model/swap_coin_data.f.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ion/app/features/wallets/model/coins_group.f.dart';
 import 'package:ion/app/features/wallets/model/network_data.f.dart';
