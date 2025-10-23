95d486bfe7522e8d0677ef31124b8f342396a6b2
ice-evander <228465633+ice-evander@users.noreply.github.com>
2025-10-23 13:09:53 +0200

    handle local media files

--- lib/app/features/video/views/components/video_thumbnail_preview.dart
+++ lib/app/features/video/views/components/video_thumbnail_preview.dart
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:blurhash_ffi/blurhash_ffi.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/image/blurhash_image_wrapper.dart';
 import 'package:ion/app/components/progress_bar/centered_loading_indicator.dart';
 import 'package:ion/app/features/components/ion_connect_network_image/ion_connect_network_image.dart';
+import 'package:ion/app/utils/url.dart';
 
 // Setting SizedBox.shrink placeholder to avoid default while screen placeholder that brings flickering
 const _placeholder = SizedBox.shrink();
@@ -27,34 +30,24 @@ class VideoThumbnailPreview extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    if (blurhash != null) {
-      return Stack(
-        fit: StackFit.expand,
-        children: [
-          Center(
-            child: AspectRatio(
-              aspectRatio: aspectRatio,
-              child: BlurhashFfi(hash: blurhash!),
-            ),
-          ),
-          if (thumbnailUrl != null && thumbnailUrl!.isNotEmpty)
-            IonConnectNetworkImage(
-              imageUrl: thumbnailUrl!,
-              authorPubkey: authorPubkey ?? '',
-              fit: fit,
-              placeholder: (_, __) => _placeholder,
-            ),
-        ],
-      );
-    }
-
-    // If no blurhash, show thumbnail directly
+    // If no thumbnail URL, show loading indicator
     if (thumbnailUrl != null && thumbnailUrl!.isNotEmpty) {
-      return IonConnectNetworkImage(
-        imageUrl: thumbnailUrl!,
-        authorPubkey: authorPubkey ?? '',
-        fit: fit,
-        placeholder: (_, __) => _placeholder,
+      // Wrap thumbnail with BlurHash background if available
+      return BlurhashImageWrapper(
+        blurhash: blurhash,
+        aspectRatio: aspectRatio,
+        child: (isNetworkUrl(thumbnailUrl!))
+            ? IonConnectNetworkImage(
+                imageUrl: thumbnailUrl!,
+                authorPubkey: authorPubkey ?? '',
+                fit: fit,
+                placeholder: (_, __) => _placeholder,
+              )
+            : Image.file(
+                File(thumbnailUrl!),
+                fit: fit,
+                errorBuilder: (context, error, stackTrace) => _placeholder,
+              ),
       );
     }
 
