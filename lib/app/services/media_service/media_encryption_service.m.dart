090e18f0e0de7830358d53285453f3cabf172177
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-10-24 14:48:13 +0300

    feat: save progress

--- lib/app/services/media_service/media_encryption_service.m.dart
+++ lib/app/services/media_service/media_encryption_service.m.dart
@@ -2,15 +2,18 @@
 
 import 'dart:convert';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:cryptography/cryptography.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
 import 'package:ion/app/features/chat/services/shared_chat_isolate.dart';
 import 'package:ion/app/features/core/model/media_type.dart';
 import 'package:ion/app/features/core/model/mime_type.dart';
+import 'package:ion/app/features/core/providers/app_lifecycle_provider.r.dart';
 import 'package:ion/app/features/core/providers/ion_connect_media_url_fallback_provider.r.dart';
 import 'package:ion/app/features/ion_connect/model/media_attachment.dart';
 import 'package:ion/app/services/compressors/brotli_compressor.r.dart';
@@ -18,6 +21,7 @@ import 'package:ion/app/services/file_cache/ion_file_cache_manager.r.dart';
 import 'package:ion/app/services/logger/logger.dart';
 import 'package:ion/app/services/media_service/media_service.m.dart';
 import 'package:ion/app/services/media_service/mime_resolver/mime_resolver.dart';
+import 'package:ion/app/utils/queue.dart';
 import 'package:mime/mime.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -25,22 +29,75 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'media_encryption_service.m.freezed.dart';
 part 'media_encryption_service.m.g.dart';
 
+@riverpod
+MediaEncryptionService mediaEncryptionService(Ref ref) {
+  final service = MediaEncryptionService(
+    fileCacheService: ref.read(fileCacheServiceProvider),
+    brotliCompressor: ref.read(brotliCompressorProvider),
+    generateMediaUrlFallback:
+        ref.read(iONConnectMediaUrlFallbackProvider.notifier).generateFallback,
+  );
+
+  final lifecycleSubscription = ref.listen<AppLifecycleState>(
+    appLifecycleProvider,
+    (previous, next) {
+      if (next != AppLifecycleState.resumed) {
+        service.cancelAllOperations();
+      }
+    },
+  );
+
+  final authSubscription = ref.listen(authProvider, (previous, next) {
+    final isAuthenticated = next.valueOrNull?.isAuthenticated ?? false;
+    if (!isAuthenticated) {
+      service.cancelAllOperations();
+    }
+  });
+
+  ref.onDispose(() {
+    authSubscription.close();
+    lifecycleSubscription.close();
+    service.cancelAllOperations();
+  });
+
+  return service;
+}
+
 class MediaEncryptionService {
   MediaEncryptionService({
     required this.fileCacheService,
     required this.brotliCompressor,
     required this.generateMediaUrlFallback,
-  });
+    int maxConcurrentOperations = 1,
+  }) : _taskQueue = ConcurrentTasksQueue(maxConcurrent: maxConcurrentOperations);
 
   final FileCacheService fileCacheService;
   final BrotliCompressor brotliCompressor;
   final Future<String?> Function(String url, {required String authorPubkey})
       generateMediaUrlFallback;
+  final ConcurrentTasksQueue _taskQueue;
+
+  /// Returns the number of media operations currently pending in the queue
+  int get pendingOperationsCount => _taskQueue.pendingTasksCount;
+
+  /// Cancels all pending media operations in the queue
+  void cancelAllOperations() {
+    _taskQueue.cancelAll();
+  }
 
   Future<File> retrieveEncryptedMedia(
     MediaAttachment attachment, {
     required String authorPubkey,
   }) async {
+    print(
+        'QQQ: Adding retrieveEncryptedMedia to queue, current queue length: ${_taskQueue.pendingTasksCount}');
+    return _taskQueue.add(() => _retrieveEncryptedMediaInternal(attachment, authorPubkey));
+  }
+
+  Future<File> _retrieveEncryptedMediaInternal(
+    MediaAttachment attachment,
+    String authorPubkey,
+  ) async {
     try {
       if (attachment.encryptionKey != null &&
           attachment.encryptionNonce != null &&
@@ -63,8 +120,10 @@ class MediaEncryptionService {
         );
         final decryptedFile = File.fromRawPath(decryptedFileBytes);
 
-        final mimeType =
-            ionMimeTypeResolver.lookup(decryptedFile.path, headerBytes: decryptedFileBytes);
+        final mimeType = ionMimeTypeResolver.lookup(
+          decryptedFile.path,
+          headerBytes: decryptedFileBytes,
+        );
 
         final fileExtension = mimeType != null ? extensionFromMime(mimeType) ?? '' : '';
 
@@ -80,6 +139,8 @@ class MediaEncryptionService {
             bytes: decompressedFile.readAsBytesSync(),
             fileExtension: fileExtension,
           );
+          print(
+              'QQQ: Retrieved and decrypted media, queue length: ${_taskQueue.pendingTasksCount}');
 
           return decryptedFile;
         } else {
@@ -88,6 +149,8 @@ class MediaEncryptionService {
             bytes: decryptedFileBytes,
             fileExtension: fileExtension,
           );
+          print(
+              'QQQ: Retrieved and decrypted media, queue length: ${_taskQueue.pendingTasksCount}');
           return decryptedFile;
         }
       } else {
@@ -100,7 +163,13 @@ class MediaEncryptionService {
     }
   }
 
-  Future<EncryptedMediaFile> encryptMediaFile(
+  Future<EncryptedMediaFile> encryptMediaFile(MediaFile mediaFile) async {
+    print(
+        'QQQ: Adding encryptMediaFile to queue, current queue length: ${_taskQueue.pendingTasksCount}');
+    return _taskQueue.add(() => _encryptMediaFileInternal(mediaFile));
+  }
+
+  Future<EncryptedMediaFile> _encryptMediaFileInternal(
     MediaFile mediaFile,
   ) async {
     final documentsDir = await getApplicationDocumentsDirectory();
@@ -140,14 +209,6 @@ class MediaEncryptionService {
     }
   }
 }
-
-@riverpod
-MediaEncryptionService mediaEncryptionService(Ref ref) => MediaEncryptionService(
-      fileCacheService: ref.read(fileCacheServiceProvider),
-      brotliCompressor: ref.read(brotliCompressorProvider),
-      generateMediaUrlFallback:
-          ref.read(iONConnectMediaUrlFallbackProvider.notifier).generateFallback,
-    );
 
 @freezed
 class EncryptedMediaFile with _$EncryptedMediaFile {
