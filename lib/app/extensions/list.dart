e7868f579465d59372ef63280b92af45fe991425
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-10-28 11:10:44 +0200

    chore: code format

--- lib/app/extensions/list.dart
+++ lib/app/extensions/list.dart
@@ -24,6 +24,7 @@ extension DeepEqualityListExtension on List<dynamic> {
   }
 
   bool containsList(List<String> target) {
-    return any((element) => element is List<String> && const ListEquality<String>().equals(element, target));
+    return any((element) =>
+        element is List<String> && const ListEquality<String>().equals(element, target));
   }
 }
