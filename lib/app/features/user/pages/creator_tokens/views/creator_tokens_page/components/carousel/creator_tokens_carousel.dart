6e25b4d83b6b17a96f3636f8ba136c0082fb1698
ion-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-10-29 15:57:46 +0100

    chore: add license

--- lib/app/features/user/pages/creator_tokens/views/creator_tokens_page/components/carousel/creator_tokens_carousel.dart
+++ lib/app/features/user/pages/creator_tokens/views/creator_tokens_page/components/carousel/creator_tokens_carousel.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
