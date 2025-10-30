777cce566edc213090231f6ac79ff63b20a185fe
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-10-30 17:41:45 +0200

    chore: wallet page: replace notification for coming soon (#2409)
    ## Description
    Wallet page. Replace notification for coming soon
    
    ## Task ID
    <!-- Add corresponding task ID here. -->
    
    ## Type of Change
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [x] Chore
    
    ## Screenshots (if applicable)
    <img width="381" height="227" alt="image"
    src="https://github.com/user-attachments/assets/87f1960d-7c53-4316-a4ab-6889d975dde2"
    />
    <img width="374" height="235" alt="image"
    src="https://github.com/user-attachments/assets/33514bc8-b803-4109-8251-1f5520a9e057"
    />

--- lib/app/features/wallets/views/pages/wallet_page/components/balance/balance_actions.dart
+++ lib/app/features/wallets/views/pages/wallet_page/components/balance/balance_actions.dart
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/button/button.dart';
+import 'package:ion/app/components/message_notification/models/message_notification.f.dart';
+import 'package:ion/app/components/message_notification/providers/message_notification_notifier_provider.r.dart';
 import 'package:ion/app/components/skeleton/skeleton.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/app/features/wallets/views/pages/nft_details/hooks/use_show_tooltip_overlay.dart';
 import 'package:ion/app/hooks/use_on_receive_funds_flow.dart';
 import 'package:ion/generated/assets.gen.dart';
 
@@ -32,27 +32,19 @@ class BalanceActions extends HookConsumerWidget {
       ref: ref,
     );
 
-    final buttonKey = useRef(GlobalKey());
-    final showTooltipOverlay = useShowTooltipOverlay(
-      targetKey: buttonKey.value,
-      text: context.i18n.wallet_buy_coming_soon,
-    );
-
-    final swapButtonKey = useRef(GlobalKey());
-    final showSwapTooltipOverlay = useShowTooltipOverlay(
-      targetKey: swapButtonKey.value,
-      text: context.i18n.wallet_swap_coming_soon,
-    );
-
     final child = Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Expanded(
           child: TextIconButton(
-            key: buttonKey.value,
             icon: Assets.svg.iconWalletBuy.icon(),
             label: context.i18n.wallet_buy,
-            onPressed: showTooltipOverlay,
+            onPressed: () => ref.read(messageNotificationNotifierProvider.notifier).show(
+                  MessageNotification(
+                    message: context.i18n.wallet_buy_coming_soon,
+                    icon: Assets.svg.iconBlockTime.icon(size: 16.0.s),
+                  ),
+                ),
             disabled: isLoading,
           ),
         ),
@@ -69,10 +61,14 @@ class BalanceActions extends HookConsumerWidget {
         SizedBox(width: 12.0.s),
         Expanded(
           child: TextIconButton(
-            key: swapButtonKey.value,
             icon: Assets.svg.iconamoonSwap.icon(color: context.theme.appColors.primaryAccent),
             label: context.i18n.wallet_swap,
-            onPressed: showSwapTooltipOverlay,
+            onPressed: () => ref.read(messageNotificationNotifierProvider.notifier).show(
+                  MessageNotification(
+                    message: context.i18n.wallet_swap_coming_soon,
+                    icon: Assets.svg.iconBlockTime.icon(size: 16.0.s),
+                  ),
+                ),
             disabled: isLoading,
             type: ButtonType.outlined,
           ),
