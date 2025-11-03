aa9be1c66b85bb01fee021aceee85ee5cd379f0f
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-11-03 17:06:15 +0200

    feat: follow button on video page (#2382)
    ## Description
    - Refactored follow button
    - Added follow button on video page
    
    ## Task ID
    ION-4245
    
    ## Type of Change
    - [ ] Bug fix
    - [X] New feature
    - [ ] Breaking change
    - [X] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    
    
    
    https://github.com/user-attachments/assets/58b93523-b397-4083-9f5e-06e7296080ba

--- lib/app/components/button/follow_button.dart
+++ lib/app/components/button/follow_button.dart
@@ -1,48 +1,133 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:ion/app/components/button/button.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/services/logger/logger.dart';
 import 'package:ion/generated/assets.gen.dart';
 
-class FollowButton extends StatelessWidget {
+class FollowButton extends HookWidget {
   const FollowButton({
     required this.onPressed,
     required this.following,
+    this.decoration,
+    this.decorationWhenFollowing,
+    this.visibility = FollowButtonVisibility.always,
     this.followLabel,
     super.key,
   });
 
-  final VoidCallback onPressed;
+  final Future<void> Function()? onPressed;
 
   final bool following;
 
   final String? followLabel;
 
+  final FollowButtonVisibility visibility;
+
+  final FollowButtonDecoration? decoration;
+
+  final FollowButtonDecoration? decorationWhenFollowing;
+
   @override
   Widget build(BuildContext context) {
-    return Button(
-      onPressed: onPressed,
-      leadingIcon: (following ? Assets.svg.iconSearchFollowers : Assets.svg.iconSearchFollow).icon(
-        color: following
-            ? context.theme.appColors.primaryAccent
-            : context.theme.appColors.onPrimaryAccent,
-        size: 16.0.s,
-      ),
-      type: following ? ButtonType.outlined : ButtonType.primary,
-      tintColor: following ? context.theme.appColors.primaryAccent : null,
-      label: Text(
-        following ? context.i18n.button_following : followLabel ?? context.i18n.button_follow,
-        style: context.theme.appTextThemes.caption.copyWith(
-          color: following
-              ? context.theme.appColors.primaryAccent
-              : context.theme.appColors.secondaryBackground,
+    final isKeptVisible = useState<bool>(false);
+    final isDisabled = useState<bool>(false);
+
+    if (visibility == FollowButtonVisibility.keepUntilRefresh) {
+      if (following && !isKeptVisible.value) {
+        return const SizedBox.shrink();
+      }
+    }
+
+    final effectiveDecoration = following
+        ? (decorationWhenFollowing ??
+            FollowButtonDecoration.defaultDecorationWhenFollowing(context))
+        : (decoration ?? FollowButtonDecoration.defaultDecoration(context));
+
+    Future<void> handlePressed() async {
+      if (visibility == FollowButtonVisibility.keepUntilRefresh) {
+        isKeptVisible.value = true;
+      }
+      isDisabled.value = true;
+      try {
+        await onPressed?.call();
+      } catch (e, s) {
+        Logger.error(e, stackTrace: s, message: 'Error following user');
+      } finally {
+        isDisabled.value = false;
+      }
+    }
+
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 250),
+      padding: effectiveDecoration.contentPadding,
+      decoration: effectiveDecoration,
+      alignment: Alignment.center,
+      curve: Curves.easeInOut,
+      child: TextButton(
+        onPressed: isDisabled.value ? null : handlePressed,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            (following ? Assets.svg.iconSearchFollowers : Assets.svg.iconSearchFollow).icon(
+              color: effectiveDecoration.foregroundColor,
+              size: 16.0.s,
+            ),
+            if (effectiveDecoration.showLabel) ...[
+              SizedBox(width: 3.0.s),
+              Text(
+                following
+                    ? context.i18n.button_following
+                    : followLabel ?? context.i18n.button_follow,
+                style: context.theme.appTextThemes.caption.copyWith(
+                  color: following
+                      ? context.theme.appColors.primaryAccent
+                      : context.theme.appColors.secondaryBackground,
+                ),
+              ),
+            ],
+          ],
         ),
-      ),
-      style: OutlinedButton.styleFrom(
-        minimumSize: Size(87.0.s, 28.0.s),
-        padding: EdgeInsets.symmetric(horizontal: 15.0.s),
       ),
     );
   }
+}
+
+class FollowButtonDecoration extends BoxDecoration {
+  FollowButtonDecoration({
+    required this.foregroundColor,
+    EdgeInsetsGeometry? contentPadding,
+    this.showLabel = true,
+    super.color,
+    super.border,
+    super.borderRadius,
+  }) : contentPadding = contentPadding ?? EdgeInsets.symmetric(horizontal: 14.0.s, vertical: 4.0.s);
+
+  factory FollowButtonDecoration.defaultDecoration(BuildContext context) {
+    return FollowButtonDecoration(
+      foregroundColor: context.theme.appColors.onPrimaryAccent,
+      color: context.theme.appColors.primaryAccent,
+      borderRadius: BorderRadius.circular(16.0.s),
+      border: Border.all(color: context.theme.appColors.primaryAccent),
+    );
+  }
+
+  factory FollowButtonDecoration.defaultDecorationWhenFollowing(BuildContext context) {
+    return FollowButtonDecoration(
+      foregroundColor: context.theme.appColors.primaryAccent,
+      color: context.theme.appColors.primaryAccent.withValues(alpha: 0),
+      borderRadius: BorderRadius.circular(16.0.s),
+      border: Border.all(color: context.theme.appColors.primaryAccent),
+    );
+  }
+
+  final Color foregroundColor;
+  final EdgeInsetsGeometry contentPadding;
+  final bool showLabel;
+}
+
+enum FollowButtonVisibility {
+  always,
+  keepUntilRefresh,
 }
