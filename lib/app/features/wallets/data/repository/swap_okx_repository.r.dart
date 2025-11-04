30af6b3e0487b9f364660d5b28251651eb69ba67
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-11-04 12:40:03 +0100

    chore: add SPDX license identifier to swap_quote_data, swap_quote_response, and swap_okx_repository files

--- lib/app/features/wallets/data/repository/swap_okx_repository.r.dart
+++ lib/app/features/wallets/data/repository/swap_okx_repository.r.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/core/providers/env_provider.r.dart';
