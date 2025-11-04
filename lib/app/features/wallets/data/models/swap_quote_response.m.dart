a3b35b87777d61ebe53d2aef0949bac6685502c6
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-11-04 12:35:44 +0100

    feat: add SwapQuoteData and SwapQuoteResponse models, enhance SwapOkxRepository with quote retrieval functionality

---
+++ lib/app/features/wallets/data/models/swap_quote_response.m.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:ion/app/features/wallets/data/models/swap_quote_data.m.dart';
+
+part 'swap_quote_response.m.freezed.dart';
+part 'swap_quote_response.m.g.dart';
+
+@freezed
+class SwapQuoteResponse with _$SwapQuoteResponse {
+  factory SwapQuoteResponse({
+    required String code,
+    @JsonKey(name: 'data') required List<SwapQuoteData> quotes,
+  }) = _SwapQuoteResponse;
+
+  factory SwapQuoteResponse.fromJson(Map<String, dynamic> json) => _$SwapQuoteResponseFromJson(json);
+}
