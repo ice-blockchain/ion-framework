60a6844caad02fb04ec271706a76a5edd6e35811
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2025-11-05 12:18:03 +0400

    chore: fix after review

--- lib/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_list.dart
+++ lib/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_list.dart
@@ -26,7 +26,7 @@ class TrendingVideosList extends StatelessWidget {
       sliver: SliverList.builder(
         itemCount: videos.length,
         itemBuilder: (BuildContext context, int index) => TrendingVideoListItem(
-          key: ValueKey(videos[index].pubkey),
+          key: ValueKey(videos[index].id),
           video: videos[index],
           itemSize: listOverlay.itemSize,
         ),
