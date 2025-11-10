cf6948c1731f3ba07d9466cb9da5267d8fa92b36
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-11-10 11:40:55 +0100

    feat: add ion_swap_client package and integrate swap functionality into SwapCoinsController

---
+++ packages/ion_swap_client/lib/ion_swap_config.dart
@@ -0,0 +1,13 @@
+class IONSwapConfig {
+  IONSwapConfig({
+    required this.okxApiKey,
+    required this.okxSignKey,
+    required this.okxPassphrase,
+    required this.okxApiUrl,
+  });
+
+  final String okxApiKey;
+  final String okxSignKey;
+  final String okxPassphrase;
+  final String okxApiUrl;
+}
