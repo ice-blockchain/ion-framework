feee884b6830ad97cddd6b641d380763413c5117
ice-evander <228465633+ice-evander@users.noreply.github.com>
2025-11-12 13:02:42 +0100

    read muted users at app startup

--- lib/app/features/core/providers/init_provider.r.dart
+++ lib/app/features/core/providers/init_provider.r.dart
@@ -20,6 +20,7 @@ import 'package:ion/app/features/ion_connect/providers/global_subscription.r.dar
 import 'package:ion/app/features/push_notifications/background/firebase_messaging_background_service.dart';
 import 'package:ion/app/features/push_notifications/providers/pushes_init_provider.r.dart';
 import 'package:ion/app/features/user/providers/account_notifications_sync_provider.r.dart';
+import 'package:ion/app/features/user/providers/muted_users_notifier.r.dart';
 import 'package:ion/app/features/user/providers/relays/user_chat_relays_sync_provider.r.dart';
 import 'package:ion/app/features/user/providers/relays/user_file_storage_relays_sync_provider.r.dart';
 import 'package:ion/app/features/user/providers/relays/user_relays_sync_provider.r.dart';
@@ -78,7 +79,8 @@ Future<void> initApp(Ref ref) async {
     ..listen(pushesInitProvider, noop)
     ..listen(globalSubscriptionProvider, (_, subscription) => subscription?.init())
     ..listen(accountNotificationsSyncProvider, noop)
-    ..listen(deepLinkHandlerProvider, noop);
+    ..listen(deepLinkHandlerProvider, noop)
+    ..read(cachedMutedUsersProvider);
 
   initFirebaseMessagingBackgroundHandler();
 
