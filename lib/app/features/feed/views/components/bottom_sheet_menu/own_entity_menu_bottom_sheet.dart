f91fd7c1124f629e564c9a2ce89728e86093f9dd
ice-evander <228465633+ice-evander@users.noreply.github.com>
2025-11-13 17:52:07 +0100

    Add bottom sheet menu

---
+++ lib/app/features/feed/views/components/bottom_sheet_menu/own_entity_menu_bottom_sheet.dart
@@ -0,0 +1,169 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/bottom_sheet_menu/bottom_sheet_menu_content.dart';
+import 'package:ion/app/components/icons/outlined_icon.dart';
+import 'package:ion/app/components/list_item/list_item.dart';
+import 'package:ion/app/extensions/asset_gen_image.dart';
+import 'package:ion/app/extensions/build_context.dart';
+import 'package:ion/app/extensions/num.dart';
+import 'package:ion/app/extensions/theme_data.dart';
+import 'package:ion/app/features/feed/data/models/delete/delete_confirmation_type.dart';
+import 'package:ion/app/features/feed/data/models/entities/article_data.f.dart';
+import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.f.dart';
+import 'package:ion/app/features/feed/data/models/entities/post_data.f.dart';
+import 'package:ion/app/features/feed/views/pages/entity_delete_confirmation_modal/entity_delete_confirmation_modal.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.f.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_entity_provider.r.dart';
+import 'package:ion/app/router/app_routes.gr.dart';
+import 'package:ion/app/router/utils/show_simple_bottom_sheet.dart';
+import 'package:ion/generated/assets.gen.dart';
+
+class OwnEntityMenuBottomSheet extends ConsumerWidget {
+  const OwnEntityMenuBottomSheet({
+    required this.eventReference,
+    this.onDelete,
+    super.key,
+  });
+
+  final EventReference eventReference;
+  final VoidCallback? onDelete;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final entity = ref.watch(ionConnectEntityProvider(eventReference: eventReference)).valueOrNull;
+
+    if (entity == null ||
+        (entity is! ModifiablePostEntity && entity is! PostEntity && entity is! ArticleEntity)) {
+      Navigator.of(context).pop();
+      return const SizedBox.shrink();
+    }
+
+    final menuItemsGroups = <List<Widget>>[];
+    final editMenuItems = <Widget>[];
+    final deleteMenuItems = <Widget>[];
+
+    // Add Edit menu item for ModifiablePostEntity
+    if (entity is ModifiablePostEntity && _isEntityEditable(entity)) {
+      editMenuItems.add(
+        ListItem(
+          onTap: () {
+            Navigator.of(context).pop();
+
+            final parentEvent = entity.data.parentEvent?.eventReference.encode();
+            final quotedEvent = entity.data.quotedEvent?.eventReference.encode();
+            final modifiedEvent = entity.toEventReference().encode();
+
+            if (parentEvent != null) {
+              EditReplyRoute(
+                parentEvent: parentEvent,
+                modifiedEvent: modifiedEvent,
+              ).push<void>(context);
+            } else if (quotedEvent != null) {
+              EditQuoteRoute(
+                quotedEvent: quotedEvent,
+                modifiedEvent: modifiedEvent,
+              ).push<void>(context);
+            } else if (parentEvent == null && quotedEvent == null) {
+              EditPostRoute(
+                modifiedEvent: modifiedEvent,
+              ).push<void>(context);
+            }
+          },
+          leading: OutlinedIcon(
+            icon: Assets.svg.iconEditLink.icon(
+              size: 20.0.s,
+              color: context.theme.appColors.primaryAccent,
+            ),
+          ),
+          title: Text(context.i18n.button_edit),
+          backgroundColor: Colors.transparent,
+        ),
+      );
+    }
+
+    // Add Edit menu item for ArticleEntity
+    if (entity is ArticleEntity && _isEntityEditable(entity)) {
+      editMenuItems.add(
+        ListItem(
+          onTap: () {
+            Navigator.of(context).pop();
+
+            final modifiedEvent = entity.toEventReference().encode();
+            EditArticleRoute(modifiedEvent: modifiedEvent).push<void>(context);
+          },
+          leading: OutlinedIcon(
+            icon: Assets.svg.iconEditLink.icon(
+              size: 20.0.s,
+              color: context.theme.appColors.primaryAccent,
+            ),
+          ),
+          title: Text(context.i18n.button_edit),
+          backgroundColor: Colors.transparent,
+        ),
+      );
+    }
+
+    if (editMenuItems.isNotEmpty) {
+      menuItemsGroups.add(editMenuItems);
+    }
+
+    // Add Delete menu item
+    deleteMenuItems.add(
+      ListItem(
+        onTap: () async {
+          Navigator.of(context).pop();
+          final confirmed = await showSimpleBottomSheet<bool>(
+            context: context,
+            child: EntityDeleteConfirmationModal(
+              eventReference: eventReference,
+              deleteConfirmationType: _getDeleteConfirmationType(entity),
+            ),
+          );
+
+          if ((confirmed ?? false) && context.mounted) {
+            onDelete?.call();
+          }
+        },
+        leading: OutlinedIcon(
+          icon: Assets.svg.iconBlockDelete.icon(
+            size: 20.0.s,
+            color: context.theme.appColors.attentionRed,
+          ),
+        ),
+        title: Text(
+          context.i18n.post_menu_delete,
+          style: context.theme.appTextThemes.body.copyWith(
+            color: context.theme.appColors.attentionRed,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+    );
+
+    menuItemsGroups.add(deleteMenuItems);
+
+    return BottomSheetMenuContent(groups: menuItemsGroups);
+  }
+
+  DeleteConfirmationType _getDeleteConfirmationType(IonConnectEntity entity) {
+    if (entity is ArticleEntity) {
+      return DeleteConfirmationType.article;
+    } else if (entity is ModifiablePostEntity && entity.data.hasVideo) {
+      return DeleteConfirmationType.video;
+    }
+    return DeleteConfirmationType.post;
+  }
+
+  bool _isEntityEditable(IonConnectEntity entity) {
+    return switch (entity) {
+      final ModifiablePostEntity post =>
+        post.data.editingEndedAt?.value.toDateTime.isAfter(DateTime.now()) ?? false,
+      final ArticleEntity article =>
+        article.data.editingEndedAt?.value.toDateTime.isAfter(DateTime.now()) ?? false,
+      _ => false,
+    };
+  }
+}
