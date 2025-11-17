0c8a6359aae391b3240821470b2525b894920824
ice-deimos <235467283+ice-deimos@users.noreply.github.com>
2025-11-17 11:58:56 +0100

    feat: Show CancelCreationModal only if document was edited

--- lib/app/features/feed/create_post/views/pages/post_form_modal/post_form_modal.dart
+++ lib/app/features/feed/create_post/views/pages/post_form_modal/post_form_modal.dart
@@ -2,8 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_quill/flutter_quill.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/back_hardware_button_interceptor/back_hardware_button_interceptor.dart';
 import 'package:ion/app/components/text_editor/text_editor.dart';
