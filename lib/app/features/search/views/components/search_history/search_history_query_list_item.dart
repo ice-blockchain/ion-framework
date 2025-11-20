44c3228b89202e63fa1818d798da4ecb911d6cbd
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-11-20 10:45:28 +0200

    feat: delete recent searches

--- lib/app/features/search/views/components/search_history/search_history_query_list_item.dart
+++ lib/app/features/search/views/components/search_history/search_history_query_list_item.dart
@@ -4,14 +4,19 @@ import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/generated/assets.gen.dart';
+import 'package:ion/app/features/search/views/components/remove_from_search_history_button/remove_from_search_history_button.dart';
 
 class SearchHistoryQueryListItem extends ConsumerWidget {
-  const SearchHistoryQueryListItem({required this.query, required this.onTap, super.key});
+  const SearchHistoryQueryListItem({
+    required this.query,
+    required this.onTap,
+    this.onDelete,
+    super.key,
+  });
 
   final String query;
-
   final VoidCallback onTap;
+  final VoidCallback? onDelete;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -26,13 +31,19 @@ class SearchHistoryQueryListItem extends ConsumerWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              query,
-              style: context.theme.appTextThemes.body2.copyWith(
-                color: context.theme.appColors.primaryText,
+            Expanded(
+              child: Text(
+                query,
+                style: context.theme.appTextThemes.body2.copyWith(
+                  color: context.theme.appColors.primaryText,
+                ),
               ),
             ),
-            Assets.svg.iconSearchHistorylink.icon(size: 20.0.s),
+            if (onDelete != null)
+              RemoveFromSearchHistoryButton(
+                onDelete: onDelete!,
+                style: RemoveFromSearchHistoryButtonStyle.inline,
+              ),
           ],
         ),
       ),
--- lib/app/features/search/views/components/search_history_user_list_item/search_history_user_list_item.dart
+++ lib/app/features/search/views/components/search_history_user_list_item/search_history_user_list_item.dart
@@ -4,16 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/components/ion_connect_avatar/ion_connect_avatar.dart';
+import 'package:ion/app/features/search/views/components/remove_from_search_history_button/remove_from_search_history_button.dart';
 import 'package:ion/app/features/search/views/components/search_history/search_list_item_loading.dart';
 import 'package:ion/app/features/user/model/user_preview_data.dart';
 import 'package:ion/app/features/user/providers/user_metadata_provider.r.dart';
 import 'package:ion/app/utils/username.dart';
 
 class SearchHistoryUserListItem extends ConsumerWidget {
-  const SearchHistoryUserListItem({required this.pubkey, required this.onTap, super.key});
+  const SearchHistoryUserListItem({
+    required this.pubkey,
+    required this.onTap,
+    this.onDelete,
+    super.key,
+  });
 
   final String pubkey;
   final VoidCallback onTap;
+  final VoidCallback? onDelete;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -22,7 +29,10 @@ class SearchHistoryUserListItem extends ConsumerWidget {
       data: (userPreviewData) => userPreviewData != null
           ? GestureDetector(
               onTap: onTap,
-              child: _UserListItem(userPreviewData: userPreviewData),
+              child: _UserListItem(
+                userPreviewData: userPreviewData,
+                onDelete: onDelete,
+              ),
             )
           : const SizedBox.shrink(),
       orElse: ListItemLoading.new,
@@ -31,9 +41,13 @@ class SearchHistoryUserListItem extends ConsumerWidget {
 }
 
 class _UserListItem extends StatelessWidget {
-  const _UserListItem({required this.userPreviewData});
+  const _UserListItem({
+    required this.userPreviewData,
+    this.onDelete,
+  });
 
   final UserPreviewEntity userPreviewData;
+  final VoidCallback? onDelete;
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +57,16 @@ class _UserListItem extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IonConnectAvatar(size: 65.0.s, masterPubkey: userPreviewData.masterPubkey),
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              IonConnectAvatar(size: 65.0.s, masterPubkey: userPreviewData.masterPubkey),
+              if (onDelete != null)
+                RemoveFromSearchHistoryButton(
+                  onDelete: onDelete!,
+                ),
+            ],
+          ),
           Column(
             mainAxisSize: MainAxisSize.min,
             children: [
