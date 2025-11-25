f598f1c6062809986ea768034ab6260c6060ff11
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-11-25 14:38:49 +0100

    chore: remove TODO comment for swap action in swap coins confirmation page and clean up whitespace in ion_identity_wallets

--- packages/ion_identity_client/lib/src/wallets/ion_identity_wallets.dart
+++ packages/ion_identity_client/lib/src/wallets/ion_identity_wallets.dart
@@ -198,7 +198,6 @@ class IONIdentityWallets {
     final userId = _extractUserIdService.extractUserId(username: username);
     return _walletViewsService.deleteWalletView(walletViewId, userId);
   }
-  
 
   Future<Map<String, dynamic>> makeTransfer(
     Wallet wallet,
