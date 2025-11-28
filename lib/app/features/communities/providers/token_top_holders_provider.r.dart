8376f7101000cf5a950762a14b2171b8a6a17912
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-11-28 11:18:01 +0200

    feat: tokenized communities part 1  - shared branch (#2591)
    ## Description
    This PR contains base package and pages related with tokenized
    communities.
    
    ## Additional Notes
    re-created based on
    https://github.com/ice-blockchain/ion-framework/pull/2516
    
    ## Task ID
    <!-- Add corresponding task ID here. -->
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

---
+++ lib/app/features/communities/providers/token_top_holders_provider.r.dart
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:async';
+
+import 'package:ion/app/services/ion_token_analytics/ion_token_analytics_client_provider.r.dart';
+import 'package:ion_token_analytics/ion_token_analytics.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'token_top_holders_provider.r.g.dart';
+
+@riverpod
+class TokenTopHolders extends _$TokenTopHolders {
+  @override
+  Stream<List<TopHolder>> build(
+    String masterPubkey, {
+    required int limit,
+  }) async* {
+    final client = await ref.watch(ionTokenAnalyticsClientProvider.future);
+    final subscription = await client.communityTokens.subscribeToTopHolders(
+      ionConnectAddress: masterPubkey,
+      limit: limit,
+    );
+
+    ref.onDispose(subscription.close);
+
+    // Buffer for the initial load
+    final initialItems = <TopHolder>[];
+    var isInitialLoad = true;
+    var currentList = <TopHolder>[];
+
+    await for (final newTopHolder in subscription.stream) {
+      if (isInitialLoad) {
+        if (newTopHolder is TopHolderPatch && newTopHolder.isEmpty()) {
+          // End of initial load
+          isInitialLoad = false;
+          currentList = List.from(initialItems);
+          // Sort by rank initially if needed
+          // currentList.sort((a, b) => a.position.rank.compareTo(b.position.rank));
+          yield currentList;
+        } else {
+          // Accumulate items
+          if (newTopHolder is TopHolder) {
+            initialItems.add(newTopHolder);
+          }
+        }
+      } else {
+        // Update phase
+        final existIndex = currentList.indexWhere(
+          (element) =>
+              element.position.addresses.ionConnect == newTopHolder.position?.addresses?.ionConnect,
+        );
+
+        if (existIndex >= 0) {
+          final existHolder = currentList[existIndex];
+          if (newTopHolder is TopHolderPatch) {
+            final patchedHolder = existHolder.merge(newTopHolder);
+
+            // Create a new list reference for state update
+            currentList = List.from(currentList);
+            currentList[existIndex] = patchedHolder;
+          } else if (newTopHolder is TopHolder) {
+            currentList = List.from(currentList);
+            currentList[existIndex] = newTopHolder;
+          }
+
+          currentList.sort((a, b) => a.position.rank.compareTo(b.position.rank));
+          yield currentList;
+        } else {
+          // New item added after initial load
+          if (newTopHolder is TopHolder) {
+            final newList = List<TopHolder>.from(currentList)
+              ..insert(0, newTopHolder)
+              ..sort((a, b) => a.position.rank.compareTo(b.position.rank));
+            yield newList;
+          }
+        }
+      }
+    }
+  }
+}
