db7c95239de8dd3b26ea3a5993cfb987164411fe
ice-deimos <235467283+ice-deimos@users.noreply.github.com>
2025-12-01 11:08:18 +0100

    feat: edit polls in post (#2523)
    ## Description
    It should be possible to edit a poll in the post after it has been
    published
    
    ## Additional Notes
    Show Cancel Creation Modal only if document was edited
    
    ## Task ID
    4455
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    
    https://github.com/user-attachments/assets/b8f4fb54-7248-4601-88d5-40533d82fad5
    
    (cherry picked from commit 85dc8d773dfff6ad241b65ee3dc0958790047389)

--- lib/app/features/feed/polls/view/components/poll_answer_item.dart
+++ lib/app/features/feed/polls/view/components/poll_answer_item.dart
@@ -12,12 +12,14 @@ import 'package:ion/generated/assets.gen.dart';
 class PollAnswerItem extends HookConsumerWidget {
   const PollAnswerItem({
     required this.index,
+    required this.readOnly,
     this.isLast = false,
     super.key,
   });
 
   final int index;
   final bool isLast;
+  final bool readOnly;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -68,6 +70,7 @@ class PollAnswerItem extends HookConsumerWidget {
                       child: IonTextField(
                         controller: textController,
                         focusNode: focusNode,
+                        enabled: !readOnly,
                         onChanged: (value) {
                           ref.read(pollDraftNotifierProvider.notifier).updateAnswer(index, value);
                         },
@@ -102,7 +105,7 @@ class PollAnswerItem extends HookConsumerWidget {
         SizedBox(
           width: 20.0.s,
           height: 20.0.s,
-          child: isLast
+          child: isLast && !readOnly
               ? GestureDetector(
                   onTap: () {
                     ref.read(pollDraftNotifierProvider.notifier).removeAnswer(index);
