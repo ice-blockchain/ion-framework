5835ecf43649229b398a752dd9f84eb2af368a55
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-12-01 15:35:02 +0200

    fix: post article editor keyboard (#2579)
    ## Description
    Fixes focus behaviour of keyboard during post and article creation.
    Removes "Scan Text" from toolbar
    
    ## Task ID
    4520
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [x] Chore
    
    (cherry picked from commit 9df84c89ece8525689eeace8562ff037e2c9f230)

--- lib/app/features/feed/create_article/views/pages/article_form_modal/components/article_main_toolbar.dart
+++ lib/app/features/feed/create_article/views/pages/article_form_modal/components/article_main_toolbar.dart
@@ -5,6 +5,8 @@ import 'package:flutter_quill/flutter_quill.dart';
 import 'package:ion/app/components/text_editor/text_editor.dart';
 import 'package:ion/app/features/feed/views/components/actions_toolbar/actions_toolbar.dart';
 import 'package:ion/app/features/feed/views/components/toolbar_buttons/toolbar_buttons.dart';
+import 'package:ion/app/services/keyboard/keyboard.dart';
+import 'package:ion/app/services/media_service/media_service.m.dart';
 
 class ArticleMainToolbar extends StatelessWidget {
   const ArticleMainToolbar({
@@ -22,10 +24,10 @@ class ArticleMainToolbar extends StatelessWidget {
   Widget build(BuildContext context) {
     return ActionsToolbar(
       actions: [
-        ToolbarMediaButton(
+        _KeyboardDismissingMediaButton(
           delegate: QuillControllerHandler(textEditorController, ValueNotifier([])),
         ),
-        ToolbarTypographyButton(
+        _KeyboardDismissingTypographyButton(
           textEditorController: textEditorController,
           onPressed: onTypographyPressed,
         ),
@@ -41,6 +43,70 @@ class ArticleMainToolbar extends StatelessWidget {
         ),
         ToolbarSeparatorButton(textEditorController: textEditorController),
       ],
+    );
+  }
+}
+
+class _KeyboardDismissingMediaButton extends StatelessWidget {
+  const _KeyboardDismissingMediaButton({
+    required this.delegate,
+  });
+
+  final ToolbarMediaButtonDelegate delegate;
+
+  @override
+  Widget build(BuildContext context) {
+    return ToolbarMediaButton(
+      delegate: _KeyboardDismissingMediaButtonDelegate(
+        delegate: delegate,
+        context: context,
+      ),
+    );
+  }
+}
+
+class _KeyboardDismissingMediaButtonDelegate extends ToolbarMediaButtonDelegate {
+  _KeyboardDismissingMediaButtonDelegate({
+    required this.delegate,
+    required this.context,
+  });
+
+  final ToolbarMediaButtonDelegate delegate;
+  final BuildContext context;
+
+  @override
+  ValueNotifier<List<MediaFile>> get attachedMediaNotifier => delegate.attachedMediaNotifier;
+
+  @override
+  void onMediaSelected(List<MediaFile>? mediaFiles) {
+    // this is needed because quill editor handles keyboard visibility a bit differently than the regular text editor
+    hideKeyboard(context);
+    delegate.onMediaSelected(mediaFiles);
+  }
+
+  @override
+  void handleSelectedMedia(List<MediaFile> files) {
+    delegate.handleSelectedMedia(files);
+  }
+}
+
+class _KeyboardDismissingTypographyButton extends StatelessWidget {
+  const _KeyboardDismissingTypographyButton({
+    required this.textEditorController,
+    required this.onPressed,
+  });
+
+  final QuillController textEditorController;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ToolbarTypographyButton(
+      textEditorController: textEditorController,
+      onPressed: () {
+        hideKeyboard(context);
+        onPressed();
+      },
     );
   }
 }
