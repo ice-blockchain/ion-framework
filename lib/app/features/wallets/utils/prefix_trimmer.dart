14fff8fd9e1b623b8f665dd874136a6b7ce00e4e
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-12-01 09:01:04 +0100

    feat: add prefix trimming for TON transfers from Ice Chrome extension (#2592)
    ## Description
    - Crop ton prefix
    
    ## Task ID
    - ION-4546
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    
    
    
    https://github.com/user-attachments/assets/2f58d0dd-b162-4759-8b73-04fc5eb0713a

--- lib/app/features/wallets/utils/prefix_trimmer.dart
+++ lib/app/features/wallets/utils/prefix_trimmer.dart
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: ice License 1.0
 
+/// Used to trim the prefix from the value for TON transfers
+/// from Ice Chrome extension
+String _icePrefixTransfer = 'ton://transfer/';
+
 String trimPrefix(
   String value, {
   String separator = ':',
 }) {
+  if (value.startsWith(_icePrefixTransfer)) {
+    return value.substring(_icePrefixTransfer.length);
+  }
+
   final parts = value.split(separator);
   if (parts.length == 1) {
     return parts.first;
