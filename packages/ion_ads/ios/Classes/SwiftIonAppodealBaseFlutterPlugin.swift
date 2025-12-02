7c48c8e04ea9128d37b927eb77688f9462e1ccc0
ice-deimos <235467283+ice-deimos@users.noreply.github.com>
2025-12-02 08:24:15 +0100

    feat: Added native appodeal plugin

---
+++ packages/ion_ads/ios/Classes/SwiftIonAppodealBaseFlutterPlugin.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Appodeal
+
+extension SwiftIonAppodealFlutterPlugin: AppodealInitializationDelegate {
+    
+    public func appodealSDKDidInitialize() {
+        channel.invokeMethod("onInitializationFinished", arguments: nil)
+    }
+}
+
+internal var nativeAdViewBinders: [String: NativeAdViewBinder] = [:]
+
+public extension Appodeal {
+    @objc class func registerNativeAdBinder(binderId: String,
+                                            nativeAdViewBinder: NativeAdViewBinder) -> Bool {
+        if nativeAdViewBinders[binderId] != nil {
+            NSLog("A NativeAdViewBinder with the following binderId already exists: %@", binderId)
+            return false
+        }
+        
+        nativeAdViewBinders[binderId] = nativeAdViewBinder
+        return true
+    }
+    
+    @objc class func unregisterNativeAdBinder(binderId: String) -> NativeAdViewBinder? {
+        let binder = nativeAdViewBinders[binderId]
+        if binder != nil {
+            nativeAdViewBinders.removeValue(forKey: binderId)
+        }
+        return binder
+    }
+}
