e0d4aaf5bee8ce34d723843ca536cc9e00c05fd2
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-12-03 00:47:22 +0200

    chore: Add SPDX license identifier to `external_address_extension.dart` and `community_token_type.dart`.

--- lib/app/features/communities/utils/external_address_extension.dart
+++ lib/app/features/communities/utils/external_address_extension.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:ion/app/features/feed/data/models/entities/article_data.f.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.f.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
