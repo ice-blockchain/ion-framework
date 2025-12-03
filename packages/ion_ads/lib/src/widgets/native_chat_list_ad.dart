81ef56f8db0de9f6a7e7943f14ad1f200390ef1f
ice-deimos <235467283+ice-deimos@users.noreply.github.com>
2025-12-03 19:00:42 +0100

    fix: Added ad components

--- packages/ion_ads/lib/src/widgets/native_chat_list_ad.dart
+++ packages/ion_ads/lib/src/widgets/native_chat_list_ad.dart
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:ion_ads/src/components/ad_choices_container.dart';
+import 'package:ion_ads/src/components/attribution_text_container.dart';
+import 'package:ion_ads/src/components/call_to_action_button.dart';
+import 'package:ion_ads/src/components/media_asset_image.dart';
+import 'package:ion_ads/src/components/star_rating.dart';
+import 'package:ion_ads/src/config/theme_data.dart';
 import 'package:ion_ads/src/models/native_ad_asset.dart';
 
 class NativeChatListAd extends StatelessWidget {
@@ -9,34 +15,28 @@ class NativeChatListAd extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return ListTile(
-      leading: ad.iconImage != null ? CircleAvatar(backgroundImage: ad.iconImage) : null,
-      title: Row(
+      leading: MediaAssetImage(path: ad.mediaAssets?.icon, width: 48, height: 48),
+      title: Text(ad.body, style: theme.textPrimary.subtitle3),
+      subtitle: Row(
         children: [
-          Expanded(child: Text(ad.title, maxLines: 1, overflow: TextOverflow.ellipsis)),
-          const SizedBox(width: 8),
-          _AdChip(text: ad.attributionText),
+          AttributionTextContainer(text: ad.attributionText),
+          const SizedBox(width: 4),
+          const AdChoicesContainer(),
+          const SizedBox(width: 4),
+          StarRating(
+            rating: 4,
+            color: theme.adsColors.onTertiaryBackground,
+            size: 12,
+          )
         ],
       ),
-      subtitle: Text(ad.body, maxLines: 2, overflow: TextOverflow.ellipsis),
-      trailing: FilledButton.tonal(onPressed: () {}, child: Text(ad.callToAction)),
-      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-    );
-  }
-}
-
-class _AdChip extends StatelessWidget {
-  const _AdChip({required this.text});
-  final String text;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey.shade200,
-        borderRadius: BorderRadius.circular(8),
+      contentPadding: EdgeInsets.zero,
+      trailing: CallToActionButton(
+        child: Icon(Icons.download_outlined, color: theme.adsColors.onPrimaryAccent),
       ),
-      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-      child: Text(text, style: Theme.of(context).textTheme.labelSmall),
     );
   }
 }
