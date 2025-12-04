bcfe1dff6b5b08dbf14796054a8911c84dc86f2b
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-12-04 20:37:14 +0700

    fix: only when authenticated

--- lib/app/features/chat/recent_chats/providers/archive_tile_visibility_provider.r.dart
+++ lib/app/features/chat/recent_chats/providers/archive_tile_visibility_provider.r.dart
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'archive_tile_visibility_provider.r.g.dart';
 
-@Riverpod(keepAlive: true)
+@riverpod
 class ArchiveTileVisibility extends _$ArchiveTileVisibility {
   @override
-  bool build() => false;
+  bool build() {
+    keepAliveWhenAuthenticated(ref);
+
+    return false;
+  }
 
   set value(bool value) {
     state = value;
