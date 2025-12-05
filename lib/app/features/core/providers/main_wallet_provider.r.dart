822b499a13d0860b8b89fc1d9adfd87e5d36ef08
ice-linus <240443007+ice-linus@users.noreply.github.com>
2025-12-05 14:35:40 +0100

    feat(auth): enhance user switching and push notification handling
    - Implemented logic to delete push subscriptions for users upon logout.
    - Added state management for showing notifications after switching accounts.
    - Enhanced the handling of push notifications to ensure users receive relevant messages.
    - Introduced a method to retrieve user public keys based on identity key names for improved account management.

--- lib/app/features/core/providers/main_wallet_provider.r.dart
+++ lib/app/features/core/providers/main_wallet_provider.r.dart
@@ -5,6 +5,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
 import 'package:ion/app/features/core/providers/wallets_provider.r.dart';
+import 'package:ion/app/services/ion_identity/ion_identity_provider.r.dart';
+import 'package:ion/app/services/logger/logger.dart';
+import 'package:ion/app/services/storage/local_storage.r.dart';
 import 'package:ion_identity_client/ion_identity.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -27,4 +30,41 @@ Future<Wallet?> mainWallet(Ref ref) async {
   }
 
   return mainWallet;
+}
+
+@riverpod
+Future<String?> userPubkeyByIdentityKeyName(
+  Ref ref,
+  String identityKeyName,
+) async {
+  const cacheKeyPrefix = 'user_pubkey_';
+
+  final sharedPrefs = await ref.read(sharedPreferencesFoundationProvider.future);
+  final cacheKey = '$cacheKeyPrefix$identityKeyName';
+  final cachedPubkey = await sharedPrefs.getString(cacheKey);
+
+  if (cachedPubkey != null && cachedPubkey.isNotEmpty) {
+    return cachedPubkey;
+  }
+
+  try {
+    final ionIdentity = await ref.read(ionIdentityProvider.future);
+    final wallets = await ionIdentity(username: identityKeyName).wallets.getWallets();
+    final mainWallet = wallets.firstWhereOrNull((Wallet wallet) => wallet.name == 'main');
+
+    if (mainWallet != null) {
+      final userPubkey = mainWallet.signingKey.publicKey;
+      await sharedPrefs.setString(cacheKey, userPubkey);
+      return userPubkey;
+    }
+  } catch (error, stackTrace) {
+    Logger.error(
+      error,
+      message: 'Failed to get user pubkey for identity key name: $identityKeyName',
+      stackTrace: stackTrace,
+    );
+    return null;
+  }
+
+  return null;
 }
