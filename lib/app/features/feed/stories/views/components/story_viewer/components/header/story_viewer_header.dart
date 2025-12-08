2225d44811366f1ba290fad8a55984147d9a8fa9
ice-linus <240443007+ice-linus@users.noreply.github.com>
2025-12-08 18:08:13 +0100

    refactor: update StoryViewerHeader layout with blur effect

--- lib/app/features/feed/stories/views/components/story_viewer/components/header/story_viewer_header.dart
+++ lib/app/features/feed/stories/views/components/story_viewer/components/header/story_viewer_header.dart
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/list_item/badges_user_list_item.dart';
@@ -55,67 +57,74 @@ class StoryViewerHeader extends ConsumerWidget {
       child: StoryOverlayContentVisibilityWrapper(
         child: GestureDetector(
           onTap: () => ProfileRoute(pubkey: currentPost.masterPubkey).push<void>(context),
-          child: BadgesUserListItem(
-            masterPubkey: currentPost.masterPubkey,
-            leading: DecoratedBox(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.s),
-                boxShadow: [
-                  BoxShadow(
-                    offset: shadow.first.offset,
-                    blurRadius: shadow.first.blurRadius,
-                    color: shadow.first.color,
-                  ),
-                ],
-              ),
-              child: IonConnectAvatar(
-                size: ListItem.defaultAvatarSize,
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(20.0.s)),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+              child: BadgesUserListItem(
                 masterPubkey: currentPost.masterPubkey,
-              ),
-            ),
-            title: Text(
-              displayName,
-              style: textThemes.subtitle3.copyWith(
-                color: onPrimaryAccent,
-                shadows: shadow,
-              ),
-              strutStyle: const StrutStyle(forceStrutHeight: true),
-            ),
-            subtitle: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  prefixUsername(username: username, context: context),
-                  style: textThemes.caption.copyWith(
+                leading: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.s),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: shadow.first.offset,
+                        blurRadius: shadow.first.blurRadius,
+                        color: shadow.first.color,
+                      ),
+                    ],
+                  ),
+                  child: IonConnectAvatar(
+                    size: ListItem.defaultAvatarSize,
+                    masterPubkey: currentPost.masterPubkey,
+                  ),
+                ),
+                title: Text(
+                  displayName,
+                  style: textThemes.subtitle3.copyWith(
                     color: onPrimaryAccent,
                     shadows: shadow,
                   ),
+                  strutStyle: const StrutStyle(forceStrutHeight: true),
                 ),
-                if (!currentPost.isDeleted) ...[
-                  SizedBox(width: 4.0.s),
-                  Text(
-                    '•',
-                    style: textThemes.caption.copyWith(
-                      color: onPrimaryAccent,
-                      shadows: shadow,
+                subtitle: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      prefixUsername(username: username, context: context),
+                      style: textThemes.caption.copyWith(
+                        color: onPrimaryAccent,
+                        shadows: shadow,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 4.0.s),
-                  TimeAgo(
-                    time: currentPost.data.publishedAt.value.toDateTime,
-                    style: textThemes.caption.copyWith(
-                      color: onPrimaryAccent,
-                      shadows: shadow,
-                    ),
-                  ),
-                ],
-              ],
+                    if (!currentPost.isDeleted) ...[
+                      SizedBox(width: 4.0.s),
+                      Text(
+                        '•',
+                        style: textThemes.caption.copyWith(
+                          color: onPrimaryAccent,
+                          shadows: shadow,
+                        ),
+                      ),
+                      SizedBox(width: 4.0.s),
+                      TimeAgo(
+                        time: currentPost.data.publishedAt.value.toDateTime,
+                        style: textThemes.caption.copyWith(
+                          color: onPrimaryAccent,
+                          shadows: shadow,
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+                trailing: HeaderActions(post: currentPost),
+                backgroundColor: context.theme.appColors.postContent.withValues(alpha: 0.50),
+                contentPadding:
+                    EdgeInsetsDirectional.symmetric(horizontal: 12.0.s, vertical: 9.0.s),
+                borderRadius: BorderRadius.all(Radius.circular(20.0.s)),
+                constraints: BoxConstraints(minHeight: 30.0.s),
+              ),
             ),
-            trailing: HeaderActions(post: currentPost),
-            backgroundColor: context.theme.appColors.postContent.withValues(alpha: 0.50),
-            contentPadding: EdgeInsetsDirectional.symmetric(horizontal: 12.0.s, vertical: 9.0.s),
-            borderRadius: BorderRadius.all(Radius.circular(20.0.s)),
-            constraints: BoxConstraints(minHeight: 30.0.s),
           ),
         ),
       ),
