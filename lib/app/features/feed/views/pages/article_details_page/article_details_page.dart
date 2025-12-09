a4fb79931802208ce74fbf1c2ba030f7ed5268bb
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-12-09 22:32:55 +0200

    fix: article scrollbar

--- lib/app/features/feed/views/pages/article_details_page/article_details_page.dart
+++ lib/app/features/feed/views/pages/article_details_page/article_details_page.dart
@@ -21,6 +21,7 @@ import 'package:ion/app/features/feed/providers/ion_connect_entity_with_counters
 import 'package:ion/app/features/feed/views/components/bottom_sheet_menu/own_post_menu_bottom_sheet.dart';
 import 'package:ion/app/features/feed/views/components/bottom_sheet_menu/post_menu_bottom_sheet.dart';
 import 'package:ion/app/features/feed/views/components/deleted_entity/deleted_entity.dart';
+import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_content_measurer.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_date_topics.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_header.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_progress_indicator.dart';
@@ -28,6 +29,7 @@ import 'package:ion/app/features/feed/views/pages/article_details_page/component
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/more_articles_from_author.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/more_articles_from_topic.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/user_biography.dart';
+import 'package:ion/app/features/feed/views/pages/article_details_page/hooks/use_article_content_height.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/hooks/use_scroll_indicator.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.f.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
@@ -53,7 +55,7 @@ class ArticleDetailsPage extends HookConsumerWidget {
     }
 
     final scrollController = useScrollController();
-    final progress = useScrollIndicator(scrollController);
+    final screenWidth = MediaQuery.sizeOf(context).width;
 
     final delta = useMemoized(
       () => parseAndConvertDelta(
@@ -74,6 +76,79 @@ class ArticleDetailsPage extends HookConsumerWidget {
       DeletedEntity(entityType: DeletedEntityType.article);
     }
 
+    List<Widget> buildContentItems({bool forMeasurement = false}) {
+      return [
+        SizedBox(height: 13.0.s),
+        ScreenSideOffset.small(
+          child: ArticleDetailsDateTopics(
+            publishedAt: articleEntity.data.publishedAt.value.toDateTime,
+            topicsNames: topicsNames,
+          ),
+        ),
+        SizedBox(height: 16.0.s),
+        if (articleEntity.isDeleted)
+          ScreenSideOffset.small(
+            child: DeletedEntity(entityType: DeletedEntityType.article),
+          )
+        else ...[
+          ArticleDetailsHeader(
+            article: articleEntity,
+          ),
+          if (articleEntity.data.content.isNotEmpty) SizedBox(height: 20.0.s),
+          ScreenSideOffset.small(
+            child: forMeasurement
+                ? ArticleContentMeasurer(
+                    content: delta,
+                    media: articleEntity.data.media,
+                  )
+                : TextEditorPreview(
+                    content: delta,
+                    media: articleEntity.data.media,
+                    authorPubkey: articleEntity.masterPubkey,
+                    enableInteractiveSelection: true,
+                    eventReference: eventReference.encode(),
+                  ),
+          ),
+        ],
+        CounterItemsFooter(eventReference: eventReference),
+        const SectionSeparator(),
+        SizedBox(height: 20.0.s),
+        ScreenSideOffset.small(
+          child: UserBiography(eventReference: eventReference),
+        ),
+        if (topicsNames.isNotEmpty) ...[
+          SizedBox(height: 20.0.s),
+          ArticleDetailsTopics(topics: topicsNames),
+        ],
+        MoreArticlesFromAuthor(eventReference: eventReference),
+        if (topics.isNotEmpty && topicsNames.isNotEmpty)
+          MoreArticlesFromTopic(
+            eventReference: eventReference,
+            topicKey: topics.first,
+            topicName: topicsNames.first,
+          ),
+        ScreenBottomOffset(),
+      ];
+    }
+
+    // Measure the full content height offscreen
+    final (contentHeight, contentMeasurer) = useContentHeight(
+      contentKey: delta,
+      contentBuilder: (key) => SizedBox(
+        key: key,
+        width: screenWidth,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: buildContentItems(forMeasurement: true),
+        ),
+      ),
+    );
+
+    final progress = useScrollIndicator(
+      scrollController,
+      totalContentHeight: contentHeight,
+    );
+
     return Scaffold(
       appBar: NavigationAppBar.screen(
         actions: [
@@ -93,70 +168,35 @@ class ArticleDetailsPage extends HookConsumerWidget {
             ),
         ],
       ),
-      body: ScrollToTopWrapper(
-        scrollController: scrollController,
-        child: Column(
-          children: [
-            ArticleDetailsProgressIndicator(progress: progress),
-            Flexible(
-              child: CustomScrollView(
-                controller: scrollController,
-                slivers: [
-                  SliverList(
-                    delegate: SliverChildListDelegate([
-                      SizedBox(height: 13.0.s),
-                      ScreenSideOffset.small(
-                        child: ArticleDetailsDateTopics(
-                          publishedAt: articleEntity.data.publishedAt.value.toDateTime,
-                          topicsNames: topicsNames,
-                        ),
-                      ),
-                      SizedBox(height: 16.0.s),
-                      if (articleEntity.isDeleted)
-                        ScreenSideOffset.small(
-                          child: DeletedEntity(entityType: DeletedEntityType.article),
-                        )
-                      else ...[
-                        ArticleDetailsHeader(
-                          article: articleEntity,
-                        ),
-                        if (articleEntity.data.content.isNotEmpty) SizedBox(height: 20.0.s),
-                        ScreenSideOffset.small(
-                          child: TextEditorPreview(
-                            content: delta,
-                            media: articleEntity.data.media,
-                            authorPubkey: articleEntity.masterPubkey,
-                            enableInteractiveSelection: true,
-                            eventReference: eventReference.encode(),
-                          ),
-                        ),
-                      ],
-                      CounterItemsFooter(eventReference: eventReference),
-                      const SectionSeparator(),
-                      SizedBox(height: 20.0.s),
-                      ScreenSideOffset.small(
-                        child: UserBiography(eventReference: eventReference),
-                      ),
-                      if (topicsNames.isNotEmpty) ...[
-                        SizedBox(height: 20.0.s),
-                        ArticleDetailsTopics(topics: topicsNames),
-                      ],
-                      MoreArticlesFromAuthor(eventReference: eventReference),
-                      if (topics.isNotEmpty && topicsNames.isNotEmpty)
-                        MoreArticlesFromTopic(
-                          eventReference: eventReference,
-                          topicKey: topics.first,
-                          topicName: topicsNames.first,
-                        ),
-                      ScreenBottomOffset(),
-                    ]),
-                  ),
-                ],
-              ),
+      body: Stack(
+        children: [
+          Offstage(
+            child: OverflowBox(
+              alignment: AlignmentDirectional.topStart,
+              maxHeight: double.infinity,
+              child: contentMeasurer,
             ),
-            const HorizontalSeparator(),
-          ],
-        ),
+          ),
+          ScrollToTopWrapper(
+            scrollController: scrollController,
+            child: Column(
+              children: [
+                ArticleDetailsProgressIndicator(progress: progress),
+                Flexible(
+                  child: CustomScrollView(
+                    controller: scrollController,
+                    slivers: [
+                      SliverList(
+                        delegate: SliverChildListDelegate(buildContentItems()),
+                      ),
+                    ],
+                  ),
+                ),
+                const HorizontalSeparator(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
