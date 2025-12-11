97a5ab211c49280f1e23fd2a117b84d1309faadf
ice-priapus <227110521+ice-priapus@users.noreply.github.com>
2025-12-11 14:17:06 +0200

    fix: swap coin selection flow (#2744)
    ## Description
    fix the swap coin selection flow
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Task ID
    ION-4716
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/wallets/views/pages/coins_flow/swap_coins/providers/swap_coins_controller_provider.r.dart
+++ lib/app/features/wallets/views/pages/coins_flow/swap_coins/providers/swap_coins_controller_provider.r.dart
@@ -96,12 +96,12 @@ class SwapCoinsController extends _$SwapCoinsController {
     );
   }
 
-  void setSellNetwork(NetworkData network) {
+  void setSellNetwork(NetworkData? network) {
     state = state.copyWith(
       sellNetwork: network,
     );
 
-    if (state.swapQuoteInfo == null) {
+    if (network != null && state.swapQuoteInfo == null) {
       _debouncedGetQuotes();
     }
   }
@@ -112,12 +112,14 @@ class SwapCoinsController extends _$SwapCoinsController {
     );
   }
 
-  void setBuyNetwork(NetworkData network) {
+  void setBuyNetwork(NetworkData? network) {
     state = state.copyWith(
       buyNetwork: network,
     );
 
-    _getQuotes();
+    if (network != null) {
+      _getQuotes();
+    }
   }
 
   void switchCoins() {
@@ -154,12 +156,18 @@ class SwapCoinsController extends _$SwapCoinsController {
       CoinSwapType.sell => state.sellCoin,
       CoinSwapType.buy => state.buyCoin,
     };
+    final previousNetwork = switch (type) {
+      CoinSwapType.sell => state.sellNetwork,
+      CoinSwapType.buy => state.buyNetwork,
+    };
 
     switch (type) {
       case CoinSwapType.sell:
         setSellCoin(coin);
+        setSellNetwork(null);
       case CoinSwapType.buy:
         setBuyCoin(coin);
+        setBuyNetwork(null);
     }
 
     final result = await selectNetworkRouteLocationBuilder();
@@ -170,19 +178,27 @@ class SwapCoinsController extends _$SwapCoinsController {
         case CoinSwapType.buy:
           setBuyNetwork(result);
       }
+      return (
+        coin: switch (type) {
+          CoinSwapType.sell => state.sellCoin,
+          CoinSwapType.buy => state.buyCoin
+        },
+        network: switch (type) {
+          CoinSwapType.sell => state.sellNetwork,
+          CoinSwapType.buy => state.buyNetwork
+        },
+      );
     } else {
       switch (type) {
         case CoinSwapType.sell:
           setSellCoin(previousCoin);
+          setSellNetwork(previousNetwork);
         case CoinSwapType.buy:
           setBuyCoin(previousCoin);
+          setBuyNetwork(previousNetwork);
       }
+      return (coin: null, network: null);
     }
-
-    return (
-      coin: state.sellCoin,
-      network: state.sellNetwork,
-    );
   }
 
   Future<SwapCoinParameters?> _buildSwapCoinParameters({
