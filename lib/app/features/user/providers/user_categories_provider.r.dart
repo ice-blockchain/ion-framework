110f51c36d62985a63bea01b2ed14ee0bb64cf32
ice-evander <228465633+ice-evander@users.noreply.github.com>
2025-12-11 09:17:31 +0100

    cherry-pick 2b68310fac6ee2c426c6a738199753d90dee77d5 [2b68310fa]

--- lib/app/features/user/providers/user_categories_provider.r.dart
+++ lib/app/features/user/providers/user_categories_provider.r.dart
@@ -6,27 +6,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'user_categories_provider.r.g.dart';
 
-// this will be final version of categories
-// optimized for performance
-const _userCategoryData = [
-  UserCategory(key: 'aviation', name: 'Aviation'),
-  UserCategory(key: 'blockchain', name: 'Blockchain'),
-  UserCategory(key: 'business', name: 'Business'),
-  UserCategory(key: 'cars', name: 'Cars'),
-  UserCategory(key: 'cryptocurrency', name: 'Cryptocurrency'),
-  UserCategory(key: 'dataScience', name: 'Data Science'),
-  UserCategory(key: 'education', name: 'Education'),
-  UserCategory(key: 'finance', name: 'Finance'),
-  UserCategory(key: 'gamer', name: 'Gamer'),
-  UserCategory(key: 'style', name: 'Style'),
-  UserCategory(key: 'restaurant', name: 'Restaurant'),
-  UserCategory(key: 'trading', name: 'Trading'),
-  UserCategory(key: 'technology', name: 'Technology'),
-  UserCategory(key: 'traveler', name: 'Traveler'),
-  UserCategory(key: 'news', name: 'News'),
-];
-
 @riverpod
 Map<String, UserCategory> userCategories(Ref ref) {
-  return {for (final category in _userCategoryData) category.key: category};
+  return {for (final category in UserCategory.values) category.key: category};
 }
