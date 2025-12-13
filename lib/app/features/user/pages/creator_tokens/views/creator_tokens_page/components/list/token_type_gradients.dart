60e5c06d3d05388c76cd3d9bf179126c29e87344
ice-phobos <236537994+ice-phobos@users.noreply.github.com>
2025-12-13 19:45:05 +0400

    feat: token type badge on categories

---
+++ lib/app/features/user/pages/creator_tokens/views/creator_tokens_page/components/list/token_type_gradients.dart
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:ion_token_analytics/ion_token_analytics.dart';
+
+// Gradients for token type indicators
+class TokenTypeGradients {
+  TokenTypeGradients._();
+
+  static const post = SweepGradient(
+    colors: [
+      Color(0xFF7D40FF),
+      Color(0xFFAC86FF),
+      Color(0xFF7D40FF),
+      Color(0xFF5A0CFF),
+      Color(0xFF7D40FF),
+    ],
+    stops: [0.00, 0.30, 0.72, 0.97, 1.00],
+    startAngle: pi / 2,
+    endAngle: pi * 5 / 2,
+  );
+
+  static const video = SweepGradient(
+    colors: [
+      Color(0xFFEA3665),
+      Color(0xFFFF3232),
+      Color(0xFFFF6C6C),
+      Color(0xFFFF8686),
+      Color(0xFFEA3665),
+    ],
+    stops: [0.00, 0.30, 0.72, 0.97, 1.00],
+    startAngle: pi / 2,
+    endAngle: pi * 5 / 2,
+  );
+
+  static const article = SweepGradient(
+    colors: [
+      Color(0xFF00AFA5),
+      Color(0xFF00AA88),
+      Color(0xFF35D487),
+      Color(0xFF00DF86),
+      Color(0xFF00AFA5),
+    ],
+    stops: [0.00, 0.30, 0.72, 0.97, 1.00],
+    startAngle: pi / 2,
+    endAngle: pi * 5 / 2,
+  );
+
+  static SweepGradient? getGradientForType(CommunityTokenType tokenType) {
+    return switch (tokenType) {
+      CommunityTokenType.post => post,
+      CommunityTokenType.video => video,
+      CommunityTokenType.article => article,
+      CommunityTokenType.profile => null,
+    };
+  }
+}
