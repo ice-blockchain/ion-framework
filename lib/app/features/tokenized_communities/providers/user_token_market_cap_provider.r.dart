9043d798381b63c1bc65c0832eadb90f7f17d030
ice-phobos <236537994+ice-phobos@users.noreply.github.com>
2025-12-17 01:48:11 +0400

    fix: check entity has token before requesting marketcap

--- lib/app/features/tokenized_communities/providers/user_token_market_cap_provider.r.dart
+++ lib/app/features/tokenized_communities/providers/user_token_market_cap_provider.r.dart
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:math';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/features/tokenized_communities/providers/token_market_info_provider.r.dart';
+import 'package:ion/app/features/tokenized_communities/providers/ion_connect_entity_has_token_provider.r.dart';
 import 'package:ion/app/features/user/providers/user_metadata_provider.r.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -14,7 +16,17 @@ Future<double?> userTokenMarketCap(Ref ref, String pubkey) async {
   );
   if (userMetadata == null) return null;
 
-  final apiAddress = userMetadata.toEventReference().toString();
-  final tokenInfo = await ref.watch(tokenMarketInfoProvider(apiAddress).future);
-  return tokenInfo?.marketData.marketCap;
+  // Check if entity has token before requesting analytics
+  final eventReference = userMetadata.toEventReference();
+  final hasToken = await ref.watch(
+    ionConnectEntityHasTokenProvider(
+      eventReference: eventReference,
+    ).future,
+  );
+  if (!hasToken) return null;
+
+  // TODO: remove this after testing
+  // TEMP: return a random market cap when the entity has a token
+  final random = Random(eventReference.toString().hashCode);
+  return random.nextDouble() * 490000 + 10000;
 }
