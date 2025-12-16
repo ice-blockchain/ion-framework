c6bd6c66cfe88ee10a443e1114363f093ed0660f
ice-phobos <236537994+ice-phobos@users.noreply.github.com>
2025-12-16 19:03:11 +0400

    fix: embed widget height & prevent mutation with text in case text is right next to embed widget without space

--- lib/app/components/text_editor/components/custom_blocks/mention/mention_inline_widget.dart
+++ lib/app/components/text_editor/components/custom_blocks/mention/mention_inline_widget.dart
@@ -24,75 +24,51 @@ class MentionInlineWidget extends StatelessWidget {
     final bg = context.theme.appColors.primaryBackground;
     final capTextColor = context.theme.appColors.onTertiaryBackground;
     final usernameColor = context.theme.appColors.darkBlue;
+    final textStyle = context.theme.appTextThemes.body2;
 
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 4.0.s, vertical: 3.0.s),
-          decoration: BoxDecoration(
-            color: bg,
-            borderRadius: BorderRadius.circular(10.0.s),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                '$mentionPrefix$username',
-                style: context.theme.appTextThemes.body2.copyWith(
-                  color: usernameColor,
-                ),
-              ),
-              SizedBox(width: 4.0.s),
-              Container(
-                padding: EdgeInsetsDirectional.fromSTEB(3.0.s, 1.0.s, 8.0.s, 2.0.s),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0.s),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Assets.svg.iconMemeMarketcap.icon(
-                      size: 12.0.s,
-                      color: capTextColor,
-                    ),
-                    SizedBox(width: 2.0.s),
-                    Text(
-                      '\$${MarketDataFormatter.formatCompactNumber(marketCap)}',
-                      style: context.theme.appTextThemes.caption2.copyWith(
-                        color: capTextColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        PositionedDirectional(
-          top: -6.0.s,
-          end: -10.0.s,
-          child: GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: onClose,
-            child: Container(
-              width: 20.0.s,
-              height: 20.0.s,
-              decoration: BoxDecoration(
-                color: bg,
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white, width: 2.0.s),
-              ),
-              child: Icon(
-                Icons.close,
-                size: 12.0.s,
-                color: capTextColor,
-              ),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 4.0.s, vertical: 1.0.s),
+      decoration: BoxDecoration(
+        color: bg,
+        borderRadius: BorderRadius.circular(10.0.s),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            '$mentionPrefix$username',
+            style: textStyle.copyWith(
+              color: usernameColor,
+              height: 1,
             ),
           ),
-        ),
-      ],
+          SizedBox(width: 4.0.s),
+          Container(
+            padding: EdgeInsetsDirectional.fromSTEB(3.0.s, 0.5.s, 8.0.s, 1.0.s),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0.s),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Assets.svg.iconMemeMarketcap.icon(
+                  size: 12.0.s,
+                  color: capTextColor,
+                ),
+                SizedBox(width: 2.0.s),
+                Text(
+                  '\$${MarketDataFormatter.formatCompactNumber(marketCap)}',
+                  style: context.theme.appTextThemes.caption2.copyWith(
+                    color: capTextColor,
+                    height: 1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
