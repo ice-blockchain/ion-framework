00e390c1075eb8c94911f17d26f91da0b7f504d8
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-12-17 15:02:54 +0100

    feature: ion bridge from ion to bsc (#2798)
    ## Description
    - Add Bridge to ION ION -> Ion BSC
    
    ## Additional Notes
    - For now transaction work but minting don't because of BE issue, so
    disabled merge for now
    
    ## Task ID
    ION-4773
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    NA

---
+++ packages/ion_swap_client/lib/models/ion_signature.m.dart
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'ion_signature.m.freezed.dart';
+part 'ion_signature.m.g.dart';
+
+@freezed
+class IonSignature with _$IonSignature {
+  factory IonSignature({
+    required String r,
+    required String s,
+    required String? encoded,
+  }) = _IonSignature;
+
+  factory IonSignature.fromJson(Map<String, dynamic> json) => _$IonSignatureFromJson(json);
+}
---
+++ packages/ion_swap_client/lib/models/ion_signature.m.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ion_signature.m.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$IonSignatureImpl _$$IonSignatureImplFromJson(Map<String, dynamic> json) =>
+    _$IonSignatureImpl(
+      r: json['r'] as String,
+      s: json['s'] as String,
+      encoded: json['encoded'] as String?,
+    );
+
+Map<String, dynamic> _$$IonSignatureImplToJson(_$IonSignatureImpl instance) =>
+    <String, dynamic>{
+      'r': instance.r,
+      's': instance.s,
+      'encoded': instance.encoded,
+    };
