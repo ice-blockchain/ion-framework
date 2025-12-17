87cbab7c60c7ddec0464eba201d467352bed64cd
ice-erebus <223226858+ice-erebus@users.noreply.github.com>
2025-12-17 18:13:57 +0100

    fix: improve fee retrieval with default fallback

--- packages/ion_swap_client/lib/utils/swap_constants.dart
+++ packages/ion_swap_client/lib/utils/swap_constants.dart
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:ion_swap_client/models/bsc_fee_data.m.dart';
+
 class SwapConstants {
   SwapConstants._();
 
   static const Duration delayAfterApproveDuration = Duration(seconds: 3);
+  static BscFeeData defaultBscFeeData = BscFeeData(
+    maxFeePerGas: BigInt.from(4500000000000),
+    maxPriorityFeePerGas: BigInt.from(4500000000000),
+  );
 }
