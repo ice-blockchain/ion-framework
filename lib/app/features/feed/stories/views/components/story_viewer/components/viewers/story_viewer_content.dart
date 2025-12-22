c2f600176d2279fc7fedaf6a546a3a9fa12adbbd
ice-deimos <235467283+ice-deimos@users.noreply.github.com>
2025-12-22 18:42:26 +0400

    feat: Story ad for full screen

--- lib/app/features/feed/stories/views/components/story_viewer/components/viewers/story_viewer_content.dart
+++ lib/app/features/feed/stories/views/components/story_viewer/components/viewers/story_viewer_content.dart
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/progress_bar/centered_loading_indicator.dart';
 import 'package:ion/app/features/core/model/media_type.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.f.dart';
+import 'package:ion/app/features/feed/stories/providers/story_image_loading_provider.r.dart';
 import 'package:ion/app/features/feed/stories/views/components/story_viewer/components/viewers/viewers.dart';
+import 'package:ion/app/hooks/use_on_init.dart';
+import 'package:ion_ads/ion_ads.dart';
 
 class StoryViewerContent extends StatelessWidget {
   const StoryViewerContent({
@@ -40,5 +44,26 @@ class StoryViewerContent extends StatelessWidget {
         ),
       _ => const CenteredLoadingIndicator(),
     };
+  }
+}
+
+class AdStoryViewer extends HookConsumerWidget {
+  const AdStoryViewer({required this.storyId, super.key});
+
+  final String storyId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    useOnInit(() {
+      if (context.mounted) {
+        ref.read(storyImageLoadStatusProvider(storyId).notifier).markLoaded();
+      }
+    });
+
+    return SizedBox.expand(
+      child: AppodealNativeAd(
+        options: NativeAdOptions.appWallOptions(),
+      ),
+    );
   }
 }
