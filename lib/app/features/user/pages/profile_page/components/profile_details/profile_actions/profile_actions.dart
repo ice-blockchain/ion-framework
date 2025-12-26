9f6ebbdcfac6d7c099d2f7c8c154e187b36f550d
ice-linus <240443007+ice-linus@users.noreply.github.com>
2025-12-26 16:59:17 +0100

    fix(profile actions): remove actions in own profiles appbar (#2959)
    ## Description
    Remove action icons in own profile
    
    ## Task ID
    ION-4732
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots
    <img width="378" height="267" alt="image"
    src="https://github.com/user-attachments/assets/696da8d7-90ae-4b82-9a63-d92d6a57840f"
    />

--- lib/app/features/user/pages/profile_page/components/profile_details/profile_actions/profile_actions.dart
+++ lib/app/features/user/pages/profile_page/components/profile_details/profile_actions/profile_actions.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
 import 'package:ion/app/features/chat/providers/user_chat_privacy_provider.r.dart';
 import 'package:ion/app/features/components/user/follow_user_button/follow_user_button.dart';
 import 'package:ion/app/features/user/model/profile_mode.dart';
@@ -38,13 +39,14 @@ class ProfileActions extends ConsumerWidget {
     final following = ref.watch(isCurrentUserFollowingSelectorProvider(pubkey));
     final canSendMessage =
         ref.watch(canSendMessageProvider(pubkey, cache: false)).valueOrNull ?? false;
+    final isOwnUserProfile = ref.watch(isCurrentUserSelectorProvider(pubkey));
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       spacing: 12.0.s,
       children: [
         if (profileMode != ProfileMode.dark) FollowUserButton(pubkey: pubkey),
-        if (!hasPrivateWallets && canSendMessage)
+        if (!isOwnUserProfile && !hasPrivateWallets && canSendMessage)
           ProfileAction(
             onPressed: () async {
               final needToEnable2FA =
@@ -56,7 +58,7 @@ class ProfileActions extends ConsumerWidget {
             assetName: Assets.svg.iconProfileTips,
             profileMode: profileMode,
           ),
-        if (canSendMessage)
+        if (!isOwnUserProfile && canSendMessage)
           ProfileAction(
             onPressed: () {
               ConversationRoute(receiverMasterPubkey: pubkey).push<void>(context);
