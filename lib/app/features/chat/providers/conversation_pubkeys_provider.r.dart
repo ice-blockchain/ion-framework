32f80462e9792b9f310edafa024b5b86c426e297
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-12-31 00:01:23 +0700

    fix: do not send transactions and messages from/to revoked device (#2879) - Read Copilot description ION-4350 - [x] Bug fix

--- lib/app/features/chat/providers/conversation_pubkeys_provider.r.dart
+++ lib/app/features/chat/providers/conversation_pubkeys_provider.r.dart
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_event_signer_provider.r.dart';
 import 'package:ion/app/features/user/providers/user_delegation_provider.r.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -13,16 +16,31 @@ class ConversationPubkeys extends _$ConversationPubkeys {
   Future<Map<String, List<String>>> fetchUsersKeys(List<String> masterPubkeys) async {
     final usersKeys = <String, List<String>>{};
 
+    final eventSigner = await ref.read(currentUserIonConnectEventSignerProvider.future);
+    if (eventSigner == null) {
+      throw EventSignerNotFoundException();
+    }
+
+    final currentUserMasterPubkey = ref.read(currentPubkeySelectorProvider);
+    if (currentUserMasterPubkey == null) {
+      throw UserMasterPubkeyNotFoundException();
+    }
+
     for (final masterPubkey in masterPubkeys) {
       final delegation = await ref.read(userDelegationProvider(masterPubkey).future);
+      if (delegation == null) continue;
 
-      if (delegation == null) {
-        continue;
+      final activeDevicePubkeys = delegation.data.activeDelegates().keys.toList();
+
+      // Only add if the current user's device is not revoked or if it's not the current user
+      final isCurrentUser = currentUserMasterPubkey == masterPubkey;
+      final isCurrentDeviceActive = activeDevicePubkeys.contains(eventSigner.publicKey);
+
+      if (!isCurrentUser || isCurrentDeviceActive) {
+        usersKeys[masterPubkey] = activeDevicePubkeys;
       }
-
-      final pubkeys = delegation.data.delegates.map((delegate) => delegate.pubkey).toList();
-      usersKeys.addAll({masterPubkey: pubkeys});
     }
+
     return usersKeys;
   }
 }
