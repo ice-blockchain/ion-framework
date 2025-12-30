a124d729f306d4a69944af22e0315959b9d1d08f
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-12-30 20:02:49 +0200

    fix(tc): global categories - update token list item to use token's imageUrl, title and format pricing correctly (#2999)
    ## Description
    
    UI Refresh: Replaced list item layout to use TokenAvatar with a token
    imageUrl and token's title.
    X Integration: Added logic to display the X (Twitter) icon badge when
    token.source.isTwitter.
    Pricing: Switched to formatPriceWithSubscript to support subscript
    formatting for small token prices.
    Formatting: Updated market cap display to use
    MarketDataFormatter.formatCompactNumber.
    Cleanup: Removed the now unused AvatarWithBadge component.
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Task ID
    <!-- Add corresponding task ID here. -->
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->
    
    
    https://github.com/user-attachments/assets/67988c5f-0746-4545-9824-0249e8fc7eaa

--- lib/app/features/user/pages/creator_tokens/views/creator_tokens_page/components/list/creator_tokens_list_item.dart
+++ lib/app/features/user/pages/creator_tokens/views/creator_tokens_page/components/list/creator_tokens_list_item.dart
@@ -3,10 +3,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/components/list_item/badges_user_list_item.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/tokenized_communities/utils/formatters.dart';
 import 'package:ion/app/features/tokenized_communities/utils/market_data_formatter.dart';
-import 'package:ion/app/features/user/pages/creator_tokens/views/creator_tokens_page/components/list/avatar_with_badge.dart';
+import 'package:ion/app/features/tokenized_communities/views/components/cards/components/token_avatar.dart';
+import 'package:ion/app/features/user/pages/creator_tokens/views/creator_tokens_page/components/list/token_type_gradients.dart';
 import 'package:ion/app/router/app_routes.gr.dart';
 import 'package:ion/app/utils/num.dart';
 import 'package:ion/generated/assets.gen.dart';
@@ -15,38 +16,89 @@ import 'package:ion_token_analytics/ion_token_analytics.dart';
 class CreatorTokensListItem extends ConsumerWidget {
   const CreatorTokensListItem({
     required this.token,
+    required this.index,
     super.key,
   });
 
   final CommunityToken token;
+  final int index;
 
   static double get itemHeight => 35.0.s;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 8.0.s),
-      child: BadgesUserListItem(
-        title: Text(token.creator.display, strutStyle: const StrutStyle(forceStrutHeight: true)),
-        trailing: _TokenPriceLabel(
-          price: token.marketData.priceUSD,
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        TokenizedCommunityRoute(
+          externalAddress: token.externalAddress,
+        ).push<void>(context);
+      },
+      child: Padding(
+        padding: EdgeInsetsDirectional.only(top: index == 0 ? 12.0.s : 6.0.s, bottom: 6.0.s),
+        child: Row(
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomEnd,
+              clipBehavior: Clip.none,
+              children: [
+                TokenAvatar(
+                  imageUrl: token.imageUrl,
+                  containerSize: Size.square(30.s),
+                  imageSize: Size.square(30.s),
+                  outerBorderRadius: 10.s,
+                  innerBorderRadius: 10.s,
+                  borderWidth: 0,
+                ),
+                if (token.source.isTwitter)
+                  PositionedDirectional(
+                    bottom: 0.s,
+                    end: -6.s,
+                    child: Container(
+                      padding: EdgeInsets.all(2.s),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1D1E20),
+                        borderRadius: BorderRadius.circular(4.0.s),
+                      ),
+                      child: Assets.svg.iconLoginXlogo.icon(
+                        size: 8.0.s,
+                        color: context.theme.appColors.secondaryBackground,
+                      ),
+                    ),
+                  )
+                else if (token.type != CommunityTokenType.profile)
+                  PositionedDirectional(
+                    end: -6.0.s,
+                    bottom: -1.0.s,
+                    child: _GradientIndicator(tokenType: token.type),
+                  ),
+              ],
+            ),
+            SizedBox(width: 8.0.s),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    token.title,
+                    overflow: TextOverflow.ellipsis,
+                    style: context.theme.appTextThemes.subtitle3,
+                    strutStyle: const StrutStyle(forceStrutHeight: true),
+                  ),
+                  _CreatorStatsWidget(
+                    marketCap: token.marketData.marketCap,
+                    volume: token.marketData.volume,
+                    holders: token.marketData.holders,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 8.0.s),
+            _TokenPriceLabel(
+              price: token.marketData.priceUSD,
+            ),
+          ],
         ),
-        subtitle: _CreatorStatsWidget(
-          marketCap: token.marketData.marketCap,
-          volume: token.marketData.volume,
-          holders: token.marketData.holders,
-        ),
-        //TODO (ice-kreios): fix this
-        masterPubkey: token.externalAddress,
-        leading: AvatarWithIndicator(
-          masterPubkey: token.externalAddress,
-          tokenType: token.type,
-        ),
-        onTap: () {
-          TokenizedCommunityRoute(
-            externalAddress: token.externalAddress,
-          ).push<void>(context);
-        },
       ),
     );
   }
@@ -75,7 +127,7 @@ class _TokenPriceLabel extends StatelessWidget {
       height: 20.0.s,
       child: Center(
         child: Text(
-          formatToCurrency(price),
+          formatPriceWithSubscript(price),
           style: context.theme.appTextThemes.caption4.copyWith(
             color: context.theme.appColors.primaryBackground,
             fontWeight: FontWeight.bold,
@@ -107,7 +159,10 @@ class _CreatorStatsWidget extends StatelessWidget {
     );
 
     final stats = [
-      (icon: Assets.svg.iconMemeMarketcap, value: formatCount(marketCap.toInt())),
+      (
+        icon: Assets.svg.iconMemeMarketcap,
+        value: MarketDataFormatter.formatCompactNumber(marketCap)
+      ),
       (icon: Assets.svg.iconMemeMarkers, value: MarketDataFormatter.formatVolume(volume)),
       (
         icon: Assets.svg.iconSearchGroups,
@@ -133,6 +188,29 @@ class _CreatorStatsWidget extends StatelessWidget {
           ],
         ],
       ],
+    );
+  }
+}
+
+class _GradientIndicator extends StatelessWidget {
+  const _GradientIndicator({
+    required this.tokenType,
+  });
+
+  final CommunityTokenType tokenType;
+
+  @override
+  Widget build(BuildContext context) {
+    final gradient = TokenTypeGradients.getGradientForType(tokenType);
+    if (gradient == null) return const SizedBox.shrink();
+
+    return Container(
+      width: 12.0.s,
+      height: 12.0.s,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: gradient,
+      ),
     );
   }
 }
