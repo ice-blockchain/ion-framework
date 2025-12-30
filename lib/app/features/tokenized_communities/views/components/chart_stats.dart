6fda9e5a4667901f9a187b772be6346d27befcd5
ice-linus <240443007+ice-linus@users.noreply.github.com>
2025-12-30 11:00:54 +0100

    fix: update KPI column colors in chart stats component

--- lib/app/features/tokenized_communities/views/components/chart_stats.dart
+++ lib/app/features/tokenized_communities/views/components/chart_stats.dart
@@ -75,12 +75,12 @@ class ChartStats extends StatelessWidget {
                 _KpiColumn(
                   title: i18n.chart_stats_buys,
                   value: buysText,
-                  valueColor: colors.primaryText,
+                  valueColor: colors.success,
                 ),
                 _KpiColumn(
                   title: i18n.chart_stats_sells,
                   value: sellsText,
-                  valueColor: hasNoSells ? colors.primaryText : colors.lossRed,
+                  valueColor: colors.lossRed,
                 ),
                 _KpiColumn(
                   title: i18n.chart_stats_net_buy,
