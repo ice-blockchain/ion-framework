6a2e18ef4f218839a2c2c8c9e1857c84e2b74aa6
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2026-01-16 15:17:48 +0200

    fix

--- lib/app/features/tokenized_communities/providers/featured_tokens_provider.r.dart
+++ lib/app/features/tokenized_communities/providers/featured_tokens_provider.r.dart
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'dart:ui';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/features/core/providers/app_lifecycle_provider.r.dart';
 import 'package:ion/app/services/ion_token_analytics/ion_token_analytics_client_provider.r.dart';
 import 'package:ion_token_analytics/ion_token_analytics.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -12,11 +9,6 @@ part 'featured_tokens_provider.r.g.dart';
 
 @riverpod
 Stream<List<CommunityToken>> featuredTokens(Ref ref) async* {
-  ref.listen(appLifecycleProvider, (previous, next) {
-    if (next == AppLifecycleState.resumed) {
-      ref.invalidateSelf();
-    }
-  });
   final client = await ref.watch(ionTokenAnalyticsClientProvider.future);
   final subscription = await client.communityTokens.subscribeToFeaturedTokens();
 
