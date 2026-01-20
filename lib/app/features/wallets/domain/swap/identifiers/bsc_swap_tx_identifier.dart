2b746d94f7d8cf8b459b4da541fc72912e0781a9
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2026-01-20 14:22:02 +0200

    chore: improve the code quality in the swap tx identifiers

--- lib/app/features/wallets/domain/swap/identifiers/bsc_swap_tx_identifier.dart
+++ lib/app/features/wallets/domain/swap/identifiers/bsc_swap_tx_identifier.dart
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:ion/app/features/wallets/domain/swap/identifiers/swap_transaction_identifier.dart';
-import 'package:ion/app/features/wallets/model/transaction_data.f.dart';
-import 'package:ion/app/services/logger/logger.dart';
 
 class BscSwapTxIdentifier extends SwapTransactionIdentifier {
   static const _bridgeContractAddress =
@@ -13,36 +11,4 @@ class BscSwapTxIdentifier extends SwapTransactionIdentifier {
 
   @override
   String get bridgeAddress => _bridgeContractAddress;
-
-  /// To-tx (ION → BSC): tx amount = swap.toAmount - (ionBridgeFee + ionMessageFee)
-  @override
-  bool isInTxAmountMatch(String expectedReceiveAmount, TransactionData tx) {
-    final (expectedAmount, txAmountValue) = parseAmounts(expectedReceiveAmount, tx);
-    if (expectedAmount == null || txAmountValue == null) {
-      Logger.log(
-        'SwapTxIdentifier[$networkId]: InTxAmount - parse failed '
-        '(expected: $expectedReceiveAmount, tx raw amount: null)',
-      );
-      return false;
-    }
-    if (expectedAmount == 0) {
-      Logger.log(
-        'SwapTxIdentifier[$networkId]: InTxAmount - expected amount is 0',
-      );
-      return false;
-    }
-
-    // ION → BSC to-tx: tx amount = expected - (bridge fee + message fee)
-    const totalFee =
-        SwapTransactionIdentifier.ionBridgeFee + SwapTransactionIdentifier.ionMessageFee;
-    final expectedTxAmount = expectedAmount - totalFee;
-    final isMatch = amountsEqual(expectedTxAmount, txAmountValue);
-
-    Logger.log(
-      'SwapTxIdentifier[$networkId]: InTxAmount - '
-      'expectedAmount: $expectedAmount, totalFee: $totalFee, '
-      'expectedAfterFee: $expectedTxAmount, txAmount: $txAmountValue, match: $isMatch',
-    );
-    return isMatch;
-  }
 }
