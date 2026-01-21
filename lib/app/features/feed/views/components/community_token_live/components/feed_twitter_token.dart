b9fa2d1a004e83002336e233494c0ad96f7b2b2e
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2026-01-21 12:25:55 +0200

    fix: Prefix Twitter-sourced token tickers with '$' (#3149)
    ## Description
    
    This PR standardizes the display of tickers for Twitter-sourced tokens
    by ensuring they are prefixed with `$` (cashtag style) instead of
    relying on username formatting protocols.
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Task ID
    <!-- Add corresponding task ID here. -->
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <img width="300" height="2622"
    alt="simulator_screenshot_BF4F94F0-1361-436F-B7BD-B652EBB392DA"
    src="https://github.com/user-attachments/assets/c1317c97-3d09-49c2-a6c0-79aae9b0eb34"
    />
    
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/feed/views/components/community_token_live/components/feed_twitter_token.dart
+++ lib/app/features/feed/views/components/community_token_live/components/feed_twitter_token.dart
@@ -7,6 +7,7 @@ import 'package:ion/app/components/skeleton/skeleton.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/feed/views/components/community_token_live/components/token_card_builder.dart';
 import 'package:ion/app/features/tokenized_communities/utils/market_data_formatter.dart';
+import 'package:ion/app/features/tokenized_communities/utils/prefix_x_token_ticker.dart';
 import 'package:ion/app/features/tokenized_communities/views/components/cards/components/token_avatar.dart';
 import 'package:ion/app/features/tokenized_communities/views/components/twitter_badge.dart';
 import 'package:ion/app/features/user/pages/profile_page/components/profile_background.dart';
@@ -135,10 +136,12 @@ class TwitterTokenHeader extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              prefixUsername(
-                username: token.marketData.ticker ?? '',
-                context: context,
-              ),
+              token.source.isTwitter
+                  ? prefixXTokenTicker(token.marketData.ticker)
+                  : prefixUsername(
+                      username: token.marketData.ticker ?? '',
+                      context: context,
+                    ),
               style: context.theme.appTextThemes.caption.copyWith(
                 color: context.theme.appColors.attentionBlock,
               ),
