bc50fea3177bc27d6107d22e4a65a93f68e95b0f
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2026-01-23 14:38:19 +0700

    fix: onTap for holdings (#3161)
    ## Description
    On tap for holding tokens
    
    ## Task ID
    ION-5182
    
    ## Type of Change
    - [x] Bug fix

--- lib/app/features/user/pages/profile_page/components/tabs/user_holdings_list_item.dart
+++ lib/app/features/user/pages/profile_page/components/tabs/user_holdings_list_item.dart
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/tokenized_communities/providers/token_market_info_provider.r.dart';
 import 'package:ion/app/features/tokenized_communities/utils/market_data_formatter.dart';
 import 'package:ion/app/features/tokenized_communities/views/components/cards/components/token_avatar.dart';
 import 'package:ion/app/features/tokenized_communities/views/components/profit_loss_indicator.dart';
 import 'package:ion/app/features/tokenized_communities/views/components/token_price_label.dart';
 import 'package:ion/app/features/tokenized_communities/views/components/token_type_gradient_indicator.dart';
 import 'package:ion/app/features/tokenized_communities/views/components/twitter_badge.dart';
+import 'package:ion/app/router/app_routes.gr.dart';
 import 'package:ion/app/utils/num.dart';
 import 'package:ion/generated/assets.gen.dart';
 import 'package:ion_token_analytics/ion_token_analytics.dart';
 
-class UserHoldingsListItem extends StatelessWidget {
+class UserHoldingsListItem extends ConsumerWidget {
   const UserHoldingsListItem({
     required this.token,
     super.key,
@@ -21,96 +24,108 @@ class UserHoldingsListItem extends StatelessWidget {
   final CommunityToken token;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final position = token.marketData.position;
+
     if (position == null) {
       return const SizedBox.shrink();
     }
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-          child: Row(
-            children: [
-              Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  TokenAvatar(
-                    imageUrl: token.imageUrl,
-                    containerSize: Size.square(30.0.s),
-                    imageSize: Size.square(30.0.s),
-                    outerBorderRadius: 10.0.s,
-                    innerBorderRadius: 10.0.s,
-                    borderWidth: 0,
-                  ),
-                  if (token.source.isTwitter)
-                    PositionedDirectional(
-                      start: 23.0.s,
-                      top: 20.0.s,
-                      child: TwitterBadge(
-                        iconSize: 8.0.s,
-                        iconColor: Colors.white,
-                        containerSize: 13.0.s,
-                      ),
-                    )
-                  else if (token.type != CommunityTokenType.profile)
-                    PositionedDirectional(
-                      end: -6.0.s,
-                      bottom: -1.0.s,
-                      child: TokenTypeGradientIndicator(tokenType: token.type),
-                    ),
-                ],
-              ),
-              SizedBox(width: 8.0.s),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
+    return GestureDetector(
+      onTap: () {
+        ref
+            .read(cachedTokenMarketInfoNotifierProvider(token.externalAddress).notifier)
+            .cacheToken(token);
+
+        TokenizedCommunityRoute(
+          externalAddress: token.externalAddress,
+        ).push<void>(context);
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Stack(
+                  clipBehavior: Clip.none,
                   children: [
-                    Text(
-                      '${token.title} #${position.rank}',
-                      style: context.theme.appTextThemes.subtitle3.copyWith(
-                        color: context.theme.appColors.primaryText,
+                    TokenAvatar(
+                      imageUrl: token.imageUrl,
+                      containerSize: Size.square(30.0.s),
+                      imageSize: Size.square(30.0.s),
+                      outerBorderRadius: 10.0.s,
+                      innerBorderRadius: 10.0.s,
+                      borderWidth: 0,
+                    ),
+                    if (token.source.isTwitter)
+                      PositionedDirectional(
+                        start: 23.0.s,
+                        top: 20.0.s,
+                        child: TwitterBadge(
+                          iconSize: 8.0.s,
+                          iconColor: Colors.white,
+                          containerSize: 13.0.s,
+                        ),
+                      )
+                    else if (token.type != CommunityTokenType.profile)
+                      PositionedDirectional(
+                        end: -6.0.s,
+                        bottom: -1.0.s,
+                        child: TokenTypeGradientIndicator(tokenType: token.type),
                       ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Row(
-                      children: [
-                        Assets.svg.iconTabsCoins.icon(
-                          size: 14.15.s,
-                          color: context.theme.appColors.quaternaryText,
-                        ),
-                        SizedBox(width: 4.0.s),
-                        Text(
-                          formatAmountCompactFromRaw(position.amount),
-                          style: context.theme.appTextThemes.caption.copyWith(
-                            color: context.theme.appColors.quaternaryText,
-                          ),
-                        ),
-                        SizedBox(width: 6.0.s),
-                        Text(
-                          '•',
-                          style: context.theme.appTextThemes.caption.copyWith(
-                            color: context.theme.appColors.quaternaryText,
-                          ),
-                        ),
-                        SizedBox(width: 6.0.s),
-                        ProfitLossIndicator(position: position),
-                      ],
-                    ),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(width: 8.0.s),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        '${token.title} #${position.rank}',
+                        style: context.theme.appTextThemes.subtitle3.copyWith(
+                          color: context.theme.appColors.primaryText,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Row(
+                        children: [
+                          Assets.svg.iconTabsCoins.icon(
+                            size: 14.15.s,
+                            color: context.theme.appColors.quaternaryText,
+                          ),
+                          SizedBox(width: 4.0.s),
+                          Text(
+                            formatAmountCompactFromRaw(position.amount),
+                            style: context.theme.appTextThemes.caption.copyWith(
+                              color: context.theme.appColors.quaternaryText,
+                            ),
+                          ),
+                          SizedBox(width: 6.0.s),
+                          Text(
+                            '•',
+                            style: context.theme.appTextThemes.caption.copyWith(
+                              color: context.theme.appColors.quaternaryText,
+                            ),
+                          ),
+                          SizedBox(width: 6.0.s),
+                          ProfitLossIndicator(position: position),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        TokenPriceLabel(
-          text: '\$${MarketDataFormatter.formatCompactNumber(position.amountUSD)}',
-        ),
-      ],
+          TokenPriceLabel(
+            text: '\$${MarketDataFormatter.formatCompactNumber(position.amountUSD)}',
+          ),
+        ],
+      ),
     );
   }
 }
