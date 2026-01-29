795d8cce3137858dc4ec553635695ba76475ae5b
ice-deimos <235467283+ice-deimos@users.noreply.github.com>
2026-01-29 13:28:48 +0100

    feat: Extract and reuse magic numbers as theme AdsSpacingExtension

--- packages/ion_ads/lib/src/config/theme_data.dart
+++ packages/ion_ads/lib/src/config/theme_data.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:ion_ads/src/config/ads_colors.dart';
 import 'package:ion_ads/src/config/ads_text_themes.dart';
+import 'package:ion_ads/src/config/theme_paddings.dart';
 
 extension AppThemeExtension on ThemeData {
   /// Usage example: Theme.of(context).adsColors;
@@ -29,4 +30,10 @@ extension AdsTextColorExtension on BuildContext {
   AdsColorsExtension get colors => theme.adsColors;
 
   AdsTextThemesExtension get textPrimary => theme.textPrimary;
+}
+
+extension AdsThemeData on ThemeData {
+  /// Provides easy access to ad-specific spacing and sizing constants.
+  AdsSpacingExtension get adsSpacing =>
+      extension<AdsSpacingExtension>() ?? AdsSpacingExtension.defaultSpacing();
 }
