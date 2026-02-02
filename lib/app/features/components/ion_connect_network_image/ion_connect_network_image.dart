bcb8d0b5f82c694cdf548a2df4e0cd70b86d686e
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2026-02-02 14:50:50 +0700

    chore: code format

--- lib/app/features/components/ion_connect_network_image/ion_connect_network_image.dart
+++ lib/app/features/components/ion_connect_network_image/ion_connect_network_image.dart
@@ -3,7 +3,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
-import  'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/image/ion_network_image.dart';
