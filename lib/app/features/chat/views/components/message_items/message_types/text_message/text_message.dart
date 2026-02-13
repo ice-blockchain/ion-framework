11555af2a7346b20fd61cae69e68023981b56c30
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2026-02-13 21:23:39 +0700

    fix: story reply alignment

--- lib/app/features/chat/views/components/message_items/message_types/text_message/text_message.dart
+++ lib/app/features/chat/views/components/message_items/message_types/text_message/text_message.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/components/text_span_builder/hooks/use_text_span_builder.dart';
 import 'package:ion/app/components/text_span_builder/text_span_builder.dart';
 import 'package:ion/app/components/url_preview/providers/url_metadata_provider.r.dart';
@@ -24,12 +25,14 @@ class TextMessage extends HookConsumerWidget {
     required this.eventMessage,
     this.margin,
     this.onTapReply,
+    this.screenOffsetSide,
     super.key,
   });
 
   final VoidCallback? onTapReply;
   final EventMessage eventMessage;
   final EdgeInsetsDirectional? margin;
+  final ScreenOffsetSide? screenOffsetSide;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -112,6 +115,7 @@ class TextMessage extends HookConsumerWidget {
       isMe: isMe,
       margin: margin,
       messageItem: messageItem,
+      screenOffsetSide: screenOffsetSide,
       contentPadding: EdgeInsets.symmetric(horizontal: 12.0.s, vertical: 12.0.s),
       child: IntrinsicWidth(
         child: Column(
