42555ada6bbfd27d5544412e6725926c80d2d889
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2026-02-18 12:24:25 +0200

    fix: cache url previews instead of keepalive mixin (#3409)
    ## Description
    - Cache Url preview metadata
    
    ## Task ID
    ION-5536
    
    ## Type of Change
    - [x] Bug fix

--- lib/app/features/chat/views/components/message_items/message_types/text_message/url_preview_block/url_preview_block.dart
+++ lib/app/features/chat/views/components/message_items/message_types/text_message/url_preview_block/url_preview_block.dart
@@ -6,6 +6,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/image/ion_network_image.dart';
 import 'package:ion/app/components/url_preview/url_preview.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/components/entities_list/list_cached_objects.dart';
 import 'package:ion/app/services/browser/browser.dart';
 import 'package:ion/app/utils/url.dart';
 import 'package:ogp_data_extract/ogp_data_extract.dart';
@@ -24,20 +25,30 @@ class UrlPreviewBlock extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    useAutomaticKeepAlive();
+    final cacheKey = useMemoized(() => normalizeUrl(url), [url]);
+    final cachedMeta = ListCachedObjects.maybeObjectOf<OgpDataWithKey>(context, cacheKey)?.meta;
+
     return GestureDetector(
       onTap: () => openDeepLinkOrInAppBrowser(url, ref),
       child: IntrinsicHeight(
         child: UrlPreview(
           url: url,
+          metaListener: (meta) {
+            if (meta != null && cachedMeta != meta) {
+              ListCachedObjects.updateObject<OgpDataWithKey>(
+                context,
+                (key: cacheKey, meta: meta),
+              );
+            }
+          },
           builder: (meta, favIconUrl) {
-            if (meta == null) {
+            if (cachedMeta == null) {
               return const SizedBox.shrink();
             }
             return Padding(
               padding: EdgeInsetsDirectional.only(top: 8.0.s),
               child: _MetaDataPreview(
-                meta: meta,
+                meta: cachedMeta,
                 favIconUrl: favIconUrl,
                 url: url,
                 isMe: isMe,
