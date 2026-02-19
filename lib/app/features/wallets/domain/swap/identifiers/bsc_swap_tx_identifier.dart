48f9a71f35278915b34b84b7c0d6cf46644e681b
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2026-02-19 11:01:08 +0200

    fix: udpate the swap transactions identifiers

--- lib/app/features/wallets/domain/swap/identifiers/bsc_swap_tx_identifier.dart
+++ lib/app/features/wallets/domain/swap/identifiers/bsc_swap_tx_identifier.dart
@@ -3,11 +3,12 @@
 import 'package:ion/app/features/wallets/domain/swap/identifiers/swap_transaction_identifier.dart';
 
 class BscSwapTxIdentifier extends SwapTransactionIdentifier {
-  static const _bridgeContractAddress = '0x0000000000000000000000000000000000000000';
+  static const _swapContracts = ['0x4A04Ba2c7e11e929d62761165d863505bAf95C7F'];
+  static const _burnAddresses = ['0x0000000000000000000000000000000000000000'];
 
   @override
   List<String> get networkIds => ['Bsc', 'BscTestnet'];
 
   @override
-  String get bridgeAddress => _bridgeContractAddress;
+  List<String> get swapIndicatorAddresses => [..._swapContracts, ..._burnAddresses];
 }
