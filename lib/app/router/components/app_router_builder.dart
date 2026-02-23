1f1d1ab168e2ec37ca73f3e1c9bad78322c8ca08
ice-hades <119406114+ice-hades@users.noreply.github.com>
2026-02-23 16:23:18 +0400

    fix: prevent crashes caused by null route animations during sheet dra… (#3463)
    ## Description
    This PR fixes a production crash reported in Sentry: TypeError: Null
    check operator used on a null value from HeroController during modal
    swipe-dismiss gestures (smooth_sheets flow).
    
    - Added SafeHeroController to guard hero gesture transitions when route
    animations are unexpectedly null;
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- lib/app/router/components/app_router_builder.dart
+++ lib/app/router/components/app_router_builder.dart
@@ -9,10 +9,13 @@ import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/debug/views/debug_rotate_gesture.dart';
 import 'package:ion/app/features/feed/global_notifications/helpers/feed_global_notifications_helper.dart';
 import 'package:ion/app/features/protect_account/secure_account/views/components/two_fa_signature_wrapper.dart';
+import 'package:ion/app/router/safe_hero_controller.dart';
 import 'package:ion/app/services/ui_event_queue/ui_event_queue_listener.dart';
 
 class AppRouterBuilder extends HookConsumerWidget {
   const AppRouterBuilder({super.key, this.child});
+
+  static final HeroController _heroController = SafeHeroController();
 
   final Widget? child;
 
@@ -29,7 +32,10 @@ class AppRouterBuilder extends HookConsumerWidget {
               child: DebugRotateGesture(
                 child: MessageNotificationWrapper(
                   child: TwoFaSignatureWrapper(
-                    child: child ?? const SizedBox.shrink(),
+                    child: HeroControllerScope(
+                      controller: _heroController,
+                      child: child ?? const SizedBox.shrink(),
+                    ),
                   ),
                 ),
               ),
