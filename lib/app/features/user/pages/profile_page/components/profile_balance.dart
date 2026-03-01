79adb52e6fcb94856cd499d38902d5936a781d40
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2026-03-01 16:55:08 +0200

    fix: enhance community token components with skeleton loading

--- lib/app/features/user/pages/profile_page/components/profile_balance.dart
+++ lib/app/features/user/pages/profile_page/components/profile_balance.dart
@@ -3,7 +3,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:ion/app/components/dividers/gradient_vertical_divider.dart';
 import 'package:ion/app/components/shapes/bottom_notch_rect_border.dart';
+import 'package:ion/app/components/skeleton/skeleton.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/views/components/community_token_live/components/token_card_builder.dart';
 import 'package:ion/app/features/tokenized_communities/utils/formatters.dart'
     as market_data_formatters;
 import 'package:ion/app/utils/formatters.dart';
@@ -14,23 +16,25 @@ class ProfileBalance extends StatelessWidget {
     required this.height,
     required this.coins,
     required this.amount,
+    required this.externalAddress,
     super.key,
   });
 
   final double height;
   final double coins;
   final double amount;
-
+  final String externalAddress;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      decoration: ShapeDecoration(
-        color: context.theme.appColors.backgroundBlue,
-        shape: const BottomNotchRectBorder(),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.s, vertical: 16.0.s),
+    return TokenCardBuilder(
+      externalAddress: externalAddress,
+      skeleton: _Skeleton(height: height),
+      builder: (token, _) => Container(
+        height: height,
+        decoration: ShapeDecoration(
+          color: context.theme.appColors.backgroundBlue,
+          shape: const BottomNotchRectBorder(),
+        ),
         child: Row(
           children: [
             Expanded(
@@ -69,6 +73,25 @@ class ProfileBalance extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Skeleton extends StatelessWidget {
+  const _Skeleton({required this.height});
+
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Skeleton(
+      child: Container(
+        height: height,
+        decoration: ShapeDecoration(
+          color: context.theme.appColors.backgroundBlue,
+          shape: const BottomNotchRectBorder(),
         ),
       ),
     );
