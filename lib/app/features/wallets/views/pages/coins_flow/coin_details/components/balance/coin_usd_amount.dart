138c7fe426fce4c9638994cb029740a1bdaebf82
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2026-03-03 17:25:02 +0200

    fix: linter

--- lib/app/features/wallets/views/pages/coins_flow/coin_details/components/balance/coin_usd_amount.dart
+++ lib/app/features/wallets/views/pages/coins_flow/coin_details/components/balance/coin_usd_amount.dart
@@ -11,9 +11,9 @@ import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
 import 'package:ion/app/features/wallets/model/balance_display_order.dart';
 import 'package:ion/app/features/wallets/providers/wallet_user_preferences/user_preferences_selectors.r.dart';
 import 'package:ion/app/features/wallets/providers/wallet_user_preferences/wallet_user_preferences_provider.r.dart';
-import 'package:ion/app/utils/crypto.dart';
 import 'package:ion/app/features/wallets/utils/format_approx_usd.dart';
-import 'package:ion/app/utils/num.dart';
+import 'package:ion/app/features/wallets/views/pages/coins_flow/coin_details/providers/balance_provider.r.dart';
+import 'package:ion/app/utils/crypto.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class CoinUsdAmount extends ConsumerWidget {
