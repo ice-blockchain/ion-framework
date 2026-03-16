d1a00aff90db904262da356ee79aca51b708fb35
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2026-03-16 13:54:34 +0200

    feat: add identity name settings item (#3672) - added new account settings item with a bottom sheet page - reused icon from login flow (actionWalletIdKey) ION-5776 - [ ] Bug fix - [X] New feature - [ ] Breaking change - [ ] Refactoring - [ ] Documentation - [ ] Chore
    https://github.com/user-attachments/assets/922835f8-c90b-419f-8324-c6d5172a955c

---
+++ lib/app/features/settings/views/identity_key_name_info_modal.dart
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:ion/app/components/card/info_card.dart';
+import 'package:ion/app/components/card/rounded_card.dart';
+import 'package:ion/app/components/copy/copy_builder.dart';
+import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
+import 'package:ion/app/extensions/asset_gen_image.dart';
+import 'package:ion/app/extensions/build_context.dart';
+import 'package:ion/app/extensions/num.dart';
+import 'package:ion/app/extensions/theme_data.dart';
+import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
+import 'package:ion/app/router/components/navigation_app_bar/navigation_close_button.dart';
+import 'package:ion/generated/assets.gen.dart';
+
+class IdentityKeyNameInfoModal extends StatelessWidget {
+  const IdentityKeyNameInfoModal({
+    this.identityKeyName,
+    super.key,
+  });
+
+  final String? identityKeyName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        NavigationAppBar.modal(
+          showBackButton: false,
+          title: Text(context.i18n.common_information),
+          actions: const [NavigationCloseButton()],
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0.s),
+          child: Column(
+            children: [
+              InfoCard(
+                iconAsset: Assets.svg.actionWalletIdKey,
+                title: context.i18n.common_identity_key_name,
+                description: context.i18n.settings_identity_key_name_description,
+              ),
+              if (identityKeyName case final identityKeyName? when identityKeyName.isNotEmpty) ...[
+                SizedBox(height: 20.0.s),
+                _IdentityKeyNameCard(identityKeyName: identityKeyName),
+              ],
+              ScreenBottomOffset(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _IdentityKeyNameCard extends StatelessWidget {
+  const _IdentityKeyNameCard({required this.identityKeyName});
+
+  final String identityKeyName;
+
+  @override
+  Widget build(BuildContext context) {
+    final borderColor = context.theme.appColors.onTertiaryFill;
+
+    return CopyBuilder(
+      defaultIcon: Assets.svg.iconBlockCopyBlue.icon(
+        size: 16.0.s,
+        color: context.theme.appColors.primaryAccent,
+      ),
+      defaultText: context.i18n.button_copy,
+      defaultBorderColor: borderColor,
+      builder: (context, onCopy, content) => RoundedCard.outlined(
+        padding: EdgeInsets.all(16.0.s),
+        borderColor: content.borderColor,
+        backgroundColor: context.theme.appColors.tertiaryBackground,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              identityKeyName,
+              textAlign: TextAlign.center,
+              style: context.theme.appTextThemes.subtitle.copyWith(
+                color: context.theme.appColors.primaryText,
+              ),
+            ),
+            SizedBox(width: 4.0.s),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () => onCopy(identityKeyName),
+              child: SizedBox.square(
+                dimension: 16.0.s,
+                child: content.icon,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
