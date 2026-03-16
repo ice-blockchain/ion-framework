64e0a259a2221cf66fff94cc1023e01257d5ee3f
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2026-03-16 13:47:29 +0200

    chore: code clean up

--- lib/app/features/chat/recent_chats/views/components/recent_chat_tile/recent_chat_tile.dart
+++ lib/app/features/chat/recent_chats/views/components/recent_chat_tile/recent_chat_tile.dart
@@ -66,7 +66,7 @@ class RecentChatTile extends HookConsumerWidget {
   final ConversationListItem conversation;
   final bool isVerified;
 
-  static double get tileHeight => 74.0.s - 0.5;
+  static double get tileHeight => 74.0.s;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
