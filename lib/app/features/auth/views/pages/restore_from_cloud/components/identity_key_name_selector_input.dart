38aaefa1f93c3edadfab32e15cb178d99fa9b782
ice-priapus <227110521+ice-priapus@users.noreply.github.com>
2026-03-18 15:14:54 +0200

    fix: recovery from cloud page ui

--- lib/app/features/auth/views/pages/restore_from_cloud/components/identity_key_name_selector_input.dart
+++ lib/app/features/auth/views/pages/restore_from_cloud/components/identity_key_name_selector_input.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ion/app/components/inputs/hooks/use_text_changed.dart';
 import 'package:ion/app/components/inputs/text_input/components/text_input_icons.dart';
 import 'package:ion/app/components/inputs/text_input/text_input.dart';
 import 'package:ion/app/extensions/extensions.dart';
@@ -11,8 +12,8 @@ class IdentityKeyNameSelectorInput extends HookWidget {
   const IdentityKeyNameSelectorInput({
     required this.textController,
     required this.isOpened,
-    required this.menuController,
     required this.menuEnabled,
+    required this.onToggleMenu,
     this.onFocused,
     this.initialValue,
     this.errorText,
@@ -22,15 +23,22 @@ class IdentityKeyNameSelectorInput extends HookWidget {
 
   final TextEditingController textController;
   final ValueNotifier<bool> isOpened;
-  final MenuController menuController;
   final ValueChanged<bool>? onFocused;
   final bool menuEnabled;
+  final VoidCallback onToggleMenu;
   final String? initialValue;
   final String? errorText;
   final ValueChanged<String>? onChanged;
 
   @override
   Widget build(BuildContext context) {
+    final showClear = useState(textController.text.isNotEmpty);
+
+    useTextChanged(
+      controller: textController,
+      onTextChanged: (text) => showClear.value = text.isNotEmpty,
+    );
+
     return TextInput(
       prefixIcon: TextInputIcons(
         hasRightDivider: true,
@@ -38,21 +46,23 @@ class IdentityKeyNameSelectorInput extends HookWidget {
       ),
       suffixIcon: menuEnabled
           ? TextInputIcons(
+              minWidth: 0,
               icons: [
-                SizedBox.square(
-                  dimension: 40.0.s,
-                  child: IconButton(
-                    icon:
-                        (isOpened.value ? Assets.svg.iconArrowUp : Assets.svg.iconArrowDown).icon(),
-                    onPressed: () {
-                      isOpened.value = !isOpened.value;
-                      if (menuController.isOpen) {
-                        menuController.close();
-                      } else {
-                        menuController.open();
-                      }
-                    },
+                if (!isOpened.value && showClear.value)
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(),
+                    onPressed: textController.clear,
+                    icon: Assets.svg.iconFieldClearall.icon(),
                   ),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  icon: (isOpened.value ? Assets.svg.iconArrowUp : Assets.svg.iconArrowDown).icon(),
+                  onPressed: onToggleMenu,
+                ),
+                SizedBox(
+                  width: 16.0.s,
                 ),
               ],
             )
